5a8d3fc0750c4e967e1e71f72243c4528e30e9a3
wujb <wujb@42624c65-7419-e447-9ad7-a502218ba35c>
2016-10-11 02:52:05 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@1948 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/aps/dao/PRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ge.aps.dao;
-
-import com.ge.aps.domain.SysRole;
-import webapp.framework.dao.GenericRepository;
-
-public interface PRoleRepository extends GenericRepository<SysRole> {
-
-}
--- src/src/main/java/com/ge/aps/dao/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ge.aps.dao;
-
-import com.ge.aps.domain.UserAccount;
-import webapp.framework.dao.GenericRepository;
-
-public interface UserAccountRepository extends GenericRepository<UserAccount> {
-    UserAccount getByLoginName(String loginName);
-    UserAccount getById(int userId);
-}
