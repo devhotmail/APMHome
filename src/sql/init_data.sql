7a9fd17a22b80de023504c156875aa29fef16dd6
wujb <wujb@42624c65-7419-e447-9ad7-a502218ba35c>
2016-10-17 03:27:53 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@1963 42624c65-7419-e447-9ad7-a502218ba35c

--- src/sql/init_data.sql
+++ src/sql/init_data.sql
@@ -1,9 +1,17 @@
-INSERT INTO user_account VALUES (1, 'admin', 'admin', '5605ee4b362e9754', NULL, NULL, NULL, '2016-10-03 11:08:56', true, true, NULL, NULL, false);
+INSERT INTO "site_info" VALUES (1, 'GE', 'GE', 'GE', 'GE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-INSERT INTO chart_config VALUES (1, 'dashboard_pie', 'select * from test where id>=990 ', 1, 'Pie', 'rating', 'rental_rate', 'rental_rate', '饼图测试', 'e', NULL, 2, 1, 'x Label', NULL, NULL, NULL, NULL, NULL, NULL, 'y Label', NULL, NULL, 0);
-INSERT INTO chart_config VALUES (2, 'dashboard_bar', 'select * from test where true :#searchFilter', 1, 'Bar', 'rating', 'date_str', 'rental_rate', '柱状图测试', 'e', NULL, 2, 1, 'x Label', NULL, NULL, NULL, NULL, NULL, NULL, 'y Label', NULL, NULL, 1);
-INSERT INTO chart_config VALUES (3, 'dashboard_line', 'select * from test where true :#searchFilter', 1, 'Line', 'rating', 'date_str', 'rental_rate', '趋势图', 'e', NULL, 2, 0, 'X', '', '', '', '', '', '', 'YY', '', '', 1);
+INSERT INTO "org_info" VALUES (1, 1, 'Digital Health', 'Digital Health');
 
+INSERT INTO "sys_role" VALUES (1, 'HospitalHead', '院长');
+INSERT INTO "sys_role" VALUES (2, 'DeptHead', '主任');
+INSERT INTO "sys_role" VALUES (3, 'DeviceHead', '设备科');
 
-INSERT INTO data_table_config VALUES (4, 'test1', 'select count(*) from chart_config', 'select * from chart_config', 1, 1);
-INSERT INTO data_table_config VALUES (5, 'test2', 'select count(*) from test', 'select * from test', 1, 1);
+INSERT INTO "user_account" VALUES (1, 1, 'admin', 'administrator', '5605ee4b362e9754', 'aps.admin@ge.com', '', 't', 't', 't', 't', 1, '2016-10-17 10:57:00');
+
+INSERT INTO "chart_config" VALUES (1, 'dashboard_pie', 'select * from demo_data where id>=990 ', 't', 'Pie', 'type', 'number1', 'number1', '饼图测试', 'e', NULL, 2, 't', 'x Label', NULL, NULL, NULL, NULL, NULL, NULL, 'y Label', NULL, NULL, 't');
+INSERT INTO "chart_config" VALUES (2, 'dashboard_bar', 'select * from demo_data where true :#searchFilter', 't', 'Bar', 'type', 'the_date', 'number3', '柱状图测试', 'e', NULL, 2, 't', 'x Label', '', '', '', '', '', '', 'y Label', '', '', 't');
+INSERT INTO "chart_config" VALUES (3, 'dashboard_line', 'select * from demo_data where true :#searchFilter', 't', 'Line', 'type', 'the_date', 'number4', '趋势图', 'e', NULL, 2, 't', 'X', '', '', '', '', '', '', 'YY', '', '', 't');
+
+INSERT INTO "data_table_config" VALUES (4, 'test1', 'select count(*) from chart_config', 'select * from chart_config', 't', 't');
+INSERT INTO "data_table_config" VALUES (5, 'test2', 'select count(*) from demo_data where 1=1 :#searchFilter', 'select * from demo_data where 1=1 :#searchFilter', 't', 't');
+INSERT INTO "data_table_config" VALUES (6, 'aaa', 'aaa', 'aaa', 'f', 't');
