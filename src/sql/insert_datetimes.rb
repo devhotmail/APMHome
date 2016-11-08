23a3bc4a089c3b4a49222a3bb37db395f859d0b0
xinpin <xinpin@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-08 05:16:58 +0000

    add data simulator for report fake data
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2091 42624c65-7419-e447-9ad7-a502218ba35c

---
+++ src/sql/insert_datetimes.rb
@@ -0,0 +1,224 @@
+require 'pg'
+require 'date'
+require 'securerandom'
+
+# configurations
+
+# DB connection info
+dbname  = "ge_apm"
+user    = "postgres"
+password  = "root"
+hostaddr  = "127.0.0.1"
+table     = "debug"
+
+# data generation
+startdate = "2011-1-16"
+enddate   = "2016-5-6"
+type      = ["CT", "MRI", "xRay", "DR", "XYZ", "ABC", "DEF", "LMN"]
+totalrecords  = 6
+# totalrecords  = 5
+
+
+
+conn = PG::Connection.new(:hostaddr => hostaddr, :dbname => dbname,
+                          :user => user, :password => password)
+
+stime = DateTime.parse(startdate).to_time
+etime = DateTime.parse(enddate).to_time
+duration  = etime.to_i - stime.to_i
+
+randnum = Random.new()
+
+
+table = "org_info"
+totalrecords = 10
+for i in 0..totalrecords
+  site_id = randnum.rand(1..2)
+  name    = "Hosptial-#{SecureRandom.uuid}"
+
+  sql = "insert into #{table} (site_id, name) values (\'#{site_id}\', \'#{name}\')"
+  puts sql
+  conn.exec(sql)
+end
+
+table = "asset_info"
+totalrecords = 300
+for i in 0..totalrecords
+  site_id = randnum.rand(1..2)
+  name    = "#{type.sample}-#{SecureRandom.uuid}"
+
+  function_type = randnum.rand(1..10)
+  hospital_id   = randnum.rand(1..10)
+
+  clinical_dept_id = randnum.rand(1..10)
+  asset_group   = randnum.rand(1..10)
+  asset_dept_id = 100
+  asset_owner_id = randnum.rand(1..5)
+  asset_owner_name  = "asset-owner-#{asset_owner_id}"
+  is_valid      = true
+  status        = randnum.rand(1..10)
+
+  randtime      = (duration * rand).to_i + stime.to_i
+  warranty_date = Time.at(randtime).strftime("%F")
+
+  randtime      = (duration * rand).to_i + stime.to_i
+  last_pm_date = Time.at(randtime).strftime("%F")
+
+  randtime      = (duration * rand).to_i + stime.to_i
+  last_metering_date = Time.at(randtime).strftime("%F")
+
+  sql = "insert into #{table} (site_id, name, function_type, hospital_id, clinical_dept_id, asset_group, asset_dept_id,
+        asset_owner_id, asset_owner_name,
+        is_valid, status, warranty_date, last_pm_date, last_metering_date) values (\'#{site_id}\', \'#{name}\',
+        \'#{function_type}\', \'#{hospital_id}\', \'#{clinical_dept_id}\', \'#{asset_group}\', \'#{asset_dept_id}\', \'#{asset_owner_id}\', \'#{asset_owner_name}\',
+        \'#{is_valid}\', \'#{status}\', \'#{warranty_date}\', \'#{last_pm_date}\', \'#{last_metering_date}\')"
+  puts sql
+  conn.exec(sql)
+end
+
+table = "asset_depreciation"
+totalrecords = 300
+for i in 0..totalrecords
+  site_id = randnum.rand(1..2)
+  asset_id = i + 1
+
+  randtime      = (duration * rand).to_i + stime.to_i
+  deprecate_date = Time.at(randtime).strftime("%F")
+  deprecate_amount = [1000, 2000, 3000, 4000, 5000].sample
+
+  sql = "insert into #{table} (site_id, asset_id, deprecate_date, deprecate_amount)
+        values (\'#{site_id}\', \'#{asset_id}\', \'#{deprecate_date}\', \'#{deprecate_amount}\')"
+  puts sql
+  conn.exec(sql)
+end
+
+table = "work_order"
+totalrecords = 50
+for i in 1..totalrecords
+  site_id   = randnum.rand(1..2)
+  hospital_id = randnum.rand(1..10)
+  asset_id  = randnum.rand(1..300)
+  name      = "repair-asset-#{asset_id}"
+  asset_name = "asset-name-#{SecureRandom.uuid}"
+  creator_id    = randnum.rand(1..10)
+  creator_name  = "creator-#{creator_id}"
+
+  randtime      = (duration * rand).to_i + stime.to_i
+  create_time   = Time.at(randtime).strftime("%F %T")
+
+  requestor_id    = randnum.rand(1..10)
+  requestor_name  = "requestor-#{requestor_id}"
+  request_time  = Time.at(randtime + randnum.rand(600..1800)).strftime("%F %T")
+  request_reason  = ["attrition", "broken", "user error", "unknown", "out of date", "planned maintenance"].sample
+
+  case_type     = randnum.rand(1..10)
+  case_sub_type = randnum.rand(1..10)
+  case_priority = randnum.rand(1..4)
+  is_internal   = [true, false].sample
+
+  current_person_id = randnum.rand(1..10)
+  current_step      = randnum.rand(1..7)
+  is_closed         = [true, false].sample
+
+  total_man_hour    = randnum.rand(1..24)
+  total_price       = randnum.rand(100.0..500.0).round(2)
+
+
+  sql = "insert into #{table} (site_id, hospital_id, asset_id, name, asset_name, creator_id, creator_name, create_time,
+        requestor_id, requestor_name, request_time, request_reason, case_type, case_sub_type,
+        case_priority, is_internal, current_person_id, current_step,
+        is_closed, total_man_hour, total_price) values (\'#{site_id}\', \'#{hospital_id}\', \'#{asset_id}\',
+        \'#{name}\', \'#{asset_name}\', \'#{creator_id}\',\'#{creator_name}\', \'#{create_time}\',
+        \'#{requestor_id}\', \'#{requestor_name}\',\'#{request_time}\', \'#{request_reason}\',
+        \'#{case_type}\', \'#{case_sub_type}\',\'#{case_priority}\', \'#{is_internal}\',
+        \'#{current_person_id}\', \'#{current_step}\', \'#{is_closed}\', \'#{total_man_hour}\', \'#{total_price}\')"
+  puts sql
+  conn.exec(sql)
+end
+
+# table = "pm_order"
+# totalrecords = 500
+# for i in 0..totalrecords
+#   site_id         = randnum.rand(1..2)
+#   asset_id        = randnum.rand(1..50)
+#   name            = "pm-order-#{asset_id}"
+#   creator_id    = randnum.rand(1..10)
+#   creator_name  = "creator-#{creator_id}"
+#
+#   randtime = (duration * rand).to_i + stime.to_i
+#   create_time = Time.at(randtime).strftime("%F %T")
+#
+#   is_finished   = [true, false].sample
+#
+#   sql = "insert into #{table} (site_id, asset_id, name, creator_id, creator_name, create_time,
+#         is_finished) values (\'#{site_id}\', \'#{asset_id}\',
+#         \'#{name}\', \'#{creator_id}\',\'#{creator_name}\', \'#{create_time}\',
+#         \'#{is_finished}\')"
+#   puts sql
+#   conn.exec(sql)
+# end
+
+# table = "work_order_step"
+# totalrecords = 500
+# for i in 0..totalrecords
+#   site_id         = randnum.rand(1..2)
+#   work_order_id   = randnum.rand(1..50)
+#   step_name       = ["start", "hospital approval", "vendor approval", "engineer onsite", "close"].sample
+#   owner_id    = randnum.rand(1..10)
+#   owner_name  = "owner-#{owner_id}"
+#
+#   randtime = (duration * rand).to_i + stime.to_i
+#   start_time = Time.at(randtime).strftime("%F %T")
+#   end_time = Time.at(randtime + randnum.rand(600..1800)).strftime("%F %T")
+#
+#
+#   sql = "insert into #{table} (site_id, work_order_id, step_name, owner_id, owner_name, start_time,
+#         end_time) values (\'#{site_id}\', \'#{work_order_id}\',
+#         \'#{step_name}\', \'#{owner_id}\',\'#{owner_name}\', \'#{start_time}\',
+#         \'#{end_time}\')"
+#   puts sql
+#   conn.exec(sql)
+# end
+
+
+table = "asset_clinical_record"
+totalrecords = 100000
+for i in 1..totalrecords
+  site_id = randnum.rand(1..2)
+  hospital_id = randnum.rand(1..10)
+  asset_id = randnum.rand(1..300)
+
+  modality_id       = randnum.rand(1..50)
+  modality_type_id  = randnum.rand(1..10)
+  modality_type     = type.sample
+  procedure_id      = randnum.rand(1..50)
+  procedure_name    = SecureRandom.hex
+  procedure_step_id = randnum.rand(1..6)
+  procedure_step_name = ["Step One", "Step Two", "Step Three", "Step Four", "Step Five"].sample
+
+  price_amount = randnum.rand(50..400)
+  inject_count = randnum.rand(600.0..1800.0).round(2)
+  expose_count = randnum.rand(200.0..400.0).round(2)
+  film_count   = randnum.rand(1..6)
+
+  randtime = (duration * rand).to_i + stime.to_i
+  puts Time.at(randtime).to_s
+  exam_date = Time.at(randtime).strftime("%F")
+  exam_stime = Time.at(randtime).strftime("%F %T")
+  exam_etime = Time.at(randtime + randnum.rand(600..1800)).strftime("%F %T")
+
+  sql = "insert into #{table} (site_id, hospital_id, asset_id, price_amount, inject_count, expose_count, film_count,
+        exam_date, exam_start_time, exam_end_time, modality_id, modality_type_id, modality_type,
+        procedure_id, procedure_name, procedure_step_id, procedure_step_name) values (\'#{site_id}\', \'#{hospital_id}\', \'#{asset_id}\',
+        \'#{price_amount}\', \'#{inject_count}\',\'#{expose_count}\', \'#{film_count}\',
+        \'#{exam_date}\', \'#{exam_stime}\', \'#{exam_etime}\', \'#{modality_id}\', \'#{modality_type_id}\', \'#{modality_type}\',
+        \'#{procedure_id}\', \'#{procedure_name}\', \'#{procedure_step_id}\',\'#{procedure_step_name}\')"
+  puts sql
+  conn.exec(sql)
+end
+
+# table = "asset_info"
+# res = conn.exec("select id, purchase_price, lifecycle from #{table} where purchase_price is null")
+# res.each do |item|
+#   conn.exec("update #{table} set purchase_price = \'#{randnum.rand(4000..8000)}\',
+#             lifecycle = \'#{randnum.rand(3..5)}\' where id = \'#{item[id]}\'")
