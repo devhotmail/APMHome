9d28e182d3c396f937a2b7fbfb4285ed9407db7b
wujb <wujb@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-10 03:18:58 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2108 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/view/DemoDataTableController.java
+++ src/src/main/java/com/ge/apm/view/DemoDataTableController.java
@@ -1,8 +1,10 @@
 package com.ge.apm.view;
 
+import com.ge.apm.view.sysutil.UserContextService;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import webapp.framework.dao.SearchFilter;
 import webapp.framework.web.WebUtil;
 import webapp.framework.web.mvc.SqlConfigurableDataController;
 
@@ -30,6 +32,8 @@ public class DemoDataTableController extends SqlConfigurableDataController {
     @Override
     public void setSearchFilter() {
         super.setSearchFilter();
+        this.searchFilterList.add(new SearchFilter("hospital_id", SearchFilter.Operator.EQ, UserContextService.getCurrentUserAccount().getHospitalId()));
+        
         DemoChartController demoChartController = WebUtil.getBean(DemoChartController.class);
         demoChartController.setSearchFilter();
     }
