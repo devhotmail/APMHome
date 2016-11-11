8f1ab08d58922b61b1d41195ce14610f4c32a641
shenqt <shenqt@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-11 03:23:21 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2124 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/view/asset/AssetFileAttachmentController.java
+++ src/src/main/java/com/ge/apm/view/asset/AssetFileAttachmentController.java
@@ -11,20 +11,12 @@ import com.ge.apm.domain.AssetFileAttachment;
 import com.ge.apm.domain.AssetInfo;
 import com.ge.apm.domain.UserAccount;
 import com.ge.apm.service.asset.AssetInfoService;
-import com.ge.apm.service.asset.FileUploadService;
+import com.ge.apm.service.asset.AttachmentFileService;
 import com.ge.apm.view.sysutil.UserContextService;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.SelectEvent;
-import webapp.framework.dao.SearchFilter;
 import webapp.framework.web.WebUtil;
 
 @ManagedBean
@@ -32,25 +24,28 @@ import webapp.framework.web.WebUtil;
 public class AssetFileAttachmentController extends JpaCRUDController<AssetFileAttachment> {
 
     AssetFileAttachmentRepository dao = null;
-    
-    FileUploadService fileService;
+
+    AttachmentFileService fileService;
 
     UserAccount currentUser;
-    
-    
+
     List<AssetInfo> assetList;
-    
+
     AssetInfo selectedAsset;
+
+    private AssetInfoService assetService;
+
     @Override
     protected void init() {
         dao = WebUtil.getBean(AssetFileAttachmentRepository.class);
-        fileService = WebUtil.getBean(FileUploadService.class);
+        assetService = WebUtil.getBean(AssetInfoService.class);
+        fileService = WebUtil.getBean(AttachmentFileService.class);
         currentUser = UserContextService.getCurrentUserAccount();
         this.filterBySite = true;
         this.setSiteFilter();
-        
+
         AssetInfoService service = (AssetInfoService) WebUtil.getBean(AssetInfoService.class);
-        assetList =  service.getAssetList();
+        assetList = service.getAssetList();
     }
 
     @Override
@@ -69,15 +64,13 @@ public class AssetFileAttachmentController extends JpaCRUDController<AssetFileAt
 
     @Override
     public List<AssetFileAttachment> getItemList() {
-        //to do: change the code if necessary
         return dao.findBySearchFilter(searchFilters);
     }
 
     @Override
     public void onBeforeNewObject(AssetFileAttachment object) {
         object.setSiteId(currentUser.getSiteId());
-        if(null!=selectedAsset)
-         selected.setAssetId(selectedAsset.getId());
+        this.selectedAsset = null;
     }
 
     public List<AssetInfo> getAssetList() {
@@ -95,86 +88,69 @@ public class AssetFileAttachmentController extends JpaCRUDController<AssetFileAt
     public void setSelectedAsset(AssetInfo selectedAsset) {
         this.selectedAsset = selectedAsset;
     }
-    
-    
-    
-/*
-    
-    
+
     @Override
-    public void onAfterNewObject(AssetFileAttachment object, boolean isOK) {
+    public void save() {
+        if (null == selectedAsset) {
+            WebUtil.addErrorMessage("Asset can not be null");
+        } else if (null == selected.getName() || selected.getName().isEmpty()) {
+            WebUtil.addErrorMessage("Please upload one file");
+        } else if (null == selected.getFileType() || selected.getFileType().isEmpty()) {
+            WebUtil.addErrorMessage("File type can not be null");
+        } else {
+            selected.setAssetId(selectedAsset.getId());
+            if (fileService.addAttachment(selected, selectedAsset.getHospitalId())) {
+                cancel();
+            }
+        }
+    }
+
+    public void cancel() {
+        this.selected = null;
+        this.selectedAsset = null;
     }
 
     @Override
-    public void onBeforeUpdateObject(AssetFileAttachment object) {
-    }
-    
-    @Override
-    public void onAfterUpdateObject(AssetFileAttachment object, boolean isOK) {
-    }
-    
-    @Override
-    public void onBeforeDeleteObject(AssetFileAttachment object) {
-    }
-    
-    @Override
-    public void onAfterDeleteObject(AssetFileAttachment object, boolean isOK) {
-    }
-    
-    @Override
-    public void onBeforeSave(AssetFileAttachment object) {
-    }
-    
-    @Override
-    public void onAfterDataChanged(){
-    };
-*/
-    
-    
-    public void handleFileUpload(FileUploadEvent event) {
-        
-        String fileName = "";
-        try {
-            fileName = new String(event.getFile().getFileName().getBytes(),"utf-8") ;
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(AssetFileAttachmentController.class.getName()).log(Level.SEVERE, null, ex);
+    public void delete() {
+        if (fileService.removeAttachment(selected)) {
+            cancel();
         }
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        String fileName = fileService.getFileName(event.getFile());
         selected.setName(fileName);
-        selected.setFileUrl("C:\\uploads/"+fileName);
-        if(fileService.uploadFile(event.getFile())){
+        selected.setFileUrl(fileService.getFileUrl() + fileName);
+        if (fileService.uploadFile(event.getFile(), selected.getFileUrl())) {
             FacesMessage message = new FacesMessage("Succesful", fileName + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
-    
-    public void chooseAsset() {
-        Map<String,Object> options = new HashMap<String, Object>();
-        options.put("resizable", false);
-        options.put("draggable", false);
-        options.put("modal", true);
-        RequestContext.getCurrentInstance().openDialog("selectAsset", options, null);
+
+    public void onAssetSelected(SelectEvent event) {
+        onServerEvent("assetSelected", event.getObject());
     }
-     
-    public void onAssetChosen(SelectEvent event) {
-        AssetInfo asset = (AssetInfo) event.getObject();
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Asset Selected", "Id:" + asset.getId());
-        FacesContext.getCurrentInstance().addMessage(null, message);
-        this.selectedAsset = asset;
-        selected.setAssetId(asset.getId());
-    }
-    
-    public void selectAssetFromDialog(AssetInfo asset){
-        RequestContext.getCurrentInstance().closeDialog(asset);
+
+    @Override
+    public void onServerEvent(String eventName, Object eventObject) {
+        AssetInfo asset = (AssetInfo) eventObject;
+        if (asset == null) {
+            return;
+        }
+
+        if (this.selected == null) {
+            return;
+        }
+        this.selected.setAssetId(asset.getId());
         this.selectedAsset = asset;
     }
-    
-    public List<AssetFileAttachment> getAttachList(Integer assetid, String type){
-        List<SearchFilter> sfs = new ArrayList();
-        sfs.addAll(this.searchFilters);
-        sfs.add(new SearchFilter("assetId",SearchFilter.Operator.EQ,assetid));
-        sfs.add(new SearchFilter("fileType",SearchFilter.Operator.EQ,type));
-        List<AssetFileAttachment> result = dao.findBySearchFilter(sfs);
-        return result;
+
+    public void onSelectedChange() {
+        selectedAsset = assetService.getAssetInfo(selected.getAssetId());
+        crudActionName = "Edit";
     }
-    
+
+    public boolean isCreatAction() {
+        return "Create".equals(crudActionName);
+    }
 }
