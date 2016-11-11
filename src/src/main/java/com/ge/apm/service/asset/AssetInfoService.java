e34632324fa80be3130bd887bab87b35444a2254
shenqt <shenqt@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-11 03:23:01 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2123 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/service/asset/AssetInfoService.java
+++ src/src/main/java/com/ge/apm/service/asset/AssetInfoService.java
@@ -6,7 +6,6 @@
 package com.ge.apm.service.asset;
 
 import com.ge.apm.dao.AssetInfoRepository;
-import com.ge.apm.dao.UserAccountRepository;
 import com.ge.apm.domain.AssetInfo;
 import com.ge.apm.domain.UserAccount;
 import com.ge.apm.view.sysutil.UserContextService;
@@ -21,28 +20,25 @@ import webapp.framework.web.WebUtil;
  *
  * @author 212579464
  */
-
 @ManagedBean
 @ApplicationScoped
 public class AssetInfoService {
+
     AssetInfoRepository assetInfoDao = WebUtil.getBean(AssetInfoRepository.class);
-    
-    
-    public String getAssetInfoName(int id) {
-        return assetInfoDao.findById(id).getName();
+
+    public String getAssetInfoName(Integer id) {
+            return assetInfoDao.findById(id).getName();
     }
 
     public AssetInfo getAssetInfo(int id) {
         return assetInfoDao.findById(id);
     }
-    
+
     public List<AssetInfo> getAssetList() {
         UserAccount currentUser = UserContextService.getCurrentUserAccount();
         List<SearchFilter> assetInfoFilters = new ArrayList<>();
         assetInfoFilters.add(new SearchFilter("siteId", SearchFilter.Operator.EQ, currentUser.getSiteId()));
         List<AssetInfo> assetList = assetInfoDao.findBySearchFilter(assetInfoFilters);
-        
-        System.out.println("======count"+ assetList.size());
         return assetList;
     }
 }
