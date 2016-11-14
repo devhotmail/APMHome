9dc2aeff982cce8fdb9dfaf3a369e020b396961a
wujb <wujb@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-14 06:10:39 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2166 42624c65-7419-e447-9ad7-a502218ba35c

---
+++ src/src/main/java/com/ge/apm/dao/FileUploadedRepository.java
@@ -0,0 +1,8 @@
+package com.ge.apm.dao;
+
+import com.ge.apm.domain.FileUploaded;
+import webapp.framework.dao.GenericRepository;
+
+public interface FileUploadedRepository extends GenericRepository<FileUploaded> {
+
+}
