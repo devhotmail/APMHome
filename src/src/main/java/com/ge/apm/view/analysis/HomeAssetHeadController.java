1f4260ea53284d14735ef991280adfe96e8b6627
chenyinghua <chenyinghua@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-17 01:24:10 +0000

    removed and integrated AssetViewInfo.java
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2201 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/view/analysis/HomeAssetHeadController.java
+++ src/src/main/java/com/ge/apm/view/analysis/HomeAssetHeadController.java
@@ -1,10 +1,7 @@
 package com.ge.apm.view.analysis;
 
 import com.ge.apm.view.sysutil.UserContextService;
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
-import com.google.common.base.Splitter;
+import com.google.common.base.*;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -70,6 +67,19 @@ public class HomeAssetHeadController extends SqlConfigurableChartController {
         queries.put("assetsInPm", "select a.name as asset_name, a.last_pm_date as pm_date from asset_info a where a.hospital_id = :#hospitalId and a.last_pm_date < (now() + interval '1 weeks') order by last_pm_date");
         queries.put("assetsInMetering", "select a.name as asset_name, a.last_metering_date as metering_date from asset_info a where a.hospital_id = 1 and a.last_pm_date < (now() + interval '2 months') order by a.last_metering_date");
         queries.put("assetsInQa", "select a.name as asset_name, a.last_qa_date as qa_date from asset_info a where a.hospital_id = 1 and a.last_qa_date < (now() + interval '2 months') order by a.last_qa_date");
+    }
+
+    @PostConstruct
+    public void init() {
+        hospitalId = UserContextService.getCurrentUserAccount().getHospitalId();
+        parameters.put("hospitalId", hospitalId);
+        initAssetsInfoInMt();
+        initAssetsStopped();
+        initAssetsWarrantyExpired();
+        initAssetsInPm();
+        initAssetsInMetering();
+        initAssetsInQa();
+        createBarModel();
     }
 
     public BarChartModel getBarModel() {
@@ -273,19 +283,6 @@ public class HomeAssetHeadController extends SqlConfigurableChartController {
         log.info("assetsInQa = {}", assetsInQa);
     }
 
-    @PostConstruct
-    public void init() {
-        hospitalId = UserContextService.getCurrentUserAccount().getHospitalId();
-        parameters.put("hospitalId", hospitalId);
-        initAssetsInfoInMt();
-        initAssetsStopped();
-        initAssetsWarrantyExpired();
-        initAssetsInPm();
-        initAssetsInMetering();
-        initAssetsInQa();
-        createBarModel();
-    }
-
     private ChartSeries initChartSeries(ImmutableMap<String, Long> pairs) {
         ChartSeries chartSeries = new ChartSeries();
         for (Map.Entry<String, Long> entry : pairs.entrySet()) {
@@ -324,4 +321,108 @@ public class HomeAssetHeadController extends SqlConfigurableChartController {
         yAxis.setMax(Longs.max(assetNumberInMt, assetNumberStopped, assetNumberWarrantyExpired, assetNumberInPm, assetNumberInMetering, assetNumberInQa));
         barModel.setExtender("skinBar");
     }
+
+
+    public static class AssetViewInfo {
+        private String asset;
+        private String stepId;
+        private String stepName;
+        private String owner;
+        private String downTime;
+        private String downReason;
+        private String warrantyDate;
+        private String pmDate;
+        private String meteringDate;
+        private String qaDate;
+
+        @Override
+        public String toString() {
+            return MoreObjects.toStringHelper(this).omitNullValues()
+                    .add("asset", asset).add("stepId", stepId).add("stepName", stepName).add("owner", owner)
+                    .add("downTime", downTime).add("downReason", downReason).add("warrantyDate", warrantyDate)
+                    .add("pmDate", pmDate).add("meteringDate", meteringDate).add("qaDate", qaDate).toString();
+        }
+
+        public String getAsset() {
+            return asset;
+        }
+
+        public void setAsset(String asset) {
+            this.asset = asset;
+        }
+
+        public String getStepId() {
+            return stepId;
+        }
+
+        public void setStepId(String stepId) {
+            this.stepId = stepId;
+        }
+
+        public String getStepName() {
+            return stepName;
+        }
+
+        public void setStepName(String stepName) {
+            this.stepName = stepName;
+        }
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        public String getDownTime() {
+            return downTime;
+        }
+
+        public void setDownTime(String downTime) {
+            this.downTime = downTime;
+        }
+
+        public String getDownReason() {
+            return downReason;
+        }
+
+        public void setDownReason(String downReason) {
+            this.downReason = downReason;
+        }
+
+        public String getWarrantyDate() {
+            return warrantyDate;
+        }
+
+        public void setWarrantyDate(String warrantyDate) {
+            this.warrantyDate = warrantyDate;
+        }
+
+        public String getPmDate() {
+            return pmDate;
+        }
+
+        public void setPmDate(String pmDate) {
+            this.pmDate = pmDate;
+        }
+
+        public String getMeteringDate() {
+            return meteringDate;
+        }
+
+        public void setMeteringDate(String meteringDate) {
+            this.meteringDate = meteringDate;
+        }
+
+        public String getQaDate() {
+            return qaDate;
+        }
+
+        public void setQaDate(String qaDate) {
+            this.qaDate = qaDate;
+        }
+    }
 }
+
+
