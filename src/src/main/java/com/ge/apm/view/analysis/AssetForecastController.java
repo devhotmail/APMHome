2971e8a273c72a0154e597f2fa554b667a92834d
xinpin <xinpin@42624c65-7419-e447-9ad7-a502218ba35c>
2016-11-24 15:05:06 +0000

    fix menu item
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2360 42624c65-7419-e447-9ad7-a502218ba35c

---
+++ src/src/main/java/com/ge/apm/view/analysis/AssetForecastController.java
@@ -0,0 +1,410 @@
+package com.ge.apm.view.analysis;
+
+import com.ge.apm.view.sysutil.UserContextService;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.primefaces.model.chart.*;
+import org.slf4j.LoggerFactory;
+import webapp.framework.dao.NativeSqlUtil;
+import webapp.framework.web.WebUtil;
+import webapp.framework.web.mvc.SqlConfigurableChartController;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.text.NumberFormat;
+import java.time.Year;
+import java.util.*;
+
+@ManagedBean
+@ViewScoped
+public class AssetForecastController extends SqlConfigurableChartController {
+    // I18n string
+    private static final String revenueStr = WebUtil.getMessage("deviceROIlg_1");
+    private static final String profitStr = WebUtil.getMessage("deviceROIlg_2");
+
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(AssetForecastController.class);
+
+    //UI Params
+    private String profitForecast = "";
+    private BarChartModel barMonthlyForecast = new BarChartModel();
+
+    // chart parameters
+    private int targetYear = Year.now().getValue();
+    private int hospitalId = 0;
+    private String selectedYear = Integer.toString(targetYear);
+
+    private String sql;
+    private HashMap<String, Object> sqlParams = new HashMap<>();
+
+    private List<Map<String, Object>> monthDep = new ArrayList<>();
+
+    private DateTime fcStartMonth = new DateTime();
+    private List<Map<String, Object>> forecastRevenue = new ArrayList<>();
+    private List<Map<String, Object>> forecastDep = new ArrayList<>();
+    private List<Map<String, Object>> forecastMaint = new ArrayList<>();
+    private List<Map<String, Object>> forecastProfit = new ArrayList<>();
+
+    private List<Map<String, Object>> predictRev = new ArrayList<>();
+    private List<Map<String, Object>> predictPro = new ArrayList<>();
+
+
+    // SQL script
+    private String depMonth =
+            "select a.hospital_id as key, " +
+                    "sum(d.deprecate_amount) as value " +
+                    "from asset_info a, asset_depreciation d " +
+                    "where a.id = d.asset_id " +
+                    "and a.hospital_id = :#hospitalId " +
+                    "group by key;";
+
+    private String forecastMonthRevenue =
+            "select sum(r.price_amount) as value, " +
+                    "to_char(r.exam_date, 'yyyy-mm') as key " +
+                    "from asset_info a, asset_clinical_record r " +
+                    "where to_char(r.exam_date, 'yyyy-mm') between to_char(now() - interval '2 year', 'yyyy-mm') " +
+                    "and to_char(now(), 'yyyy-mm') " +
+                    "and a.id = r.asset_id " +
+                    "and a.hospital_id = :#hospitalId " +
+                    "group by key order by key;";
+
+    private String forecastMonthMaint =
+            "select to_char(w.request_time, 'yyyy-mm') as key, " +
+                    "sum(w.total_price) as value " +
+                    "from asset_info a, work_order w " +
+                    "where to_char(w.request_time, 'yyyy-mm') " +
+                    "between to_char(now() - interval '2 year', 'yyyy-mm') " +
+                    "and to_char(now(), 'yyyy-mm') " +
+                    "and a.id = w.asset_id and w.is_closed = true " +
+                    "and a.hospital_id = :#hospitalId " +
+                    "group by key order by key;";
+
+    @PostConstruct
+    protected void init() {
+
+        // init sql params
+        hospitalId = UserContextService.getCurrentUserAccount().getHospitalId();
+        sqlParams.put("hospitalId", hospitalId);
+        sqlParams.put("targetYear", this.targetYear);
+
+        queryForecastProfit();
+
+        createForecastBar();
+    }
+
+    private void queryForecastProfit() {
+        double value = 0.0;
+
+        fcStartMonth = fcStartMonth.withYear(fcStartMonth.getYear() - 2);
+
+        forecastRevenue = queryForecastRevenue();
+        forecastDep = queryForecastDep();
+        forecastMaint = queryForecastMaint();
+
+        forecastProfit = calcProfit(forecastRevenue, forecastDep, forecastMaint);
+
+        predict(forecastRevenue, forecastProfit, fcStartMonth);
+
+        for (Map<String, Object> item : predictPro) {
+            value = value + (double) item.get("value");
+        }
+
+        NumberFormat cf = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        profitForecast = cf.format(value);
+
+        forecastRevenue.addAll(predictRev);
+        forecastProfit.addAll(predictPro);
+
+    }
+
+    private void predict(List<Map<String, Object>> revenue, List<Map<String, Object>> profit, DateTime startMonth) {
+        int offset = revenue.size() / 2;
+        DateTime predictMonth = startMonth;
+        predictMonth = predictMonth.plusMonths(revenue.size());
+
+        for (int index = 0; index < offset; index++) {
+            Map<String, Object> item = predictNextItem(revenue.get(index), revenue.get(index + offset), predictMonth);
+            predictRev.add(item);
+
+            item = predictNextItem(profit.get(index), profit.get(index + offset), predictMonth);
+            predictPro.add(item);
+
+            predictMonth = predictMonth.plusMonths(1);
+        }
+    }
+
+    private Map<String, Object> predictNextItem(Map<String, Object> last, Map<String, Object> recent, DateTime month) {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM");
+        Map<String, Object> item    = new HashMap<>();
+        double value = 0.0;
+        double result = 0.0;
+
+        double lastValue = (double) last.get("value");
+        double recentValue = (double) recent.get("value");
+
+        if ((recentValue > - 0.00001) && (recentValue < 0.00001)) {
+            result = 0.0;
+        } else {
+            result = (recentValue - lastValue) / recentValue;
+        }
+
+        if (result > 0.05) {
+
+            value = recentValue * (1 + result);
+
+        } else if ((result > 0) && (result < 0.05)) {
+            // assume we will be better to reach 5%
+            value = recentValue * (1 + 0.05);
+
+        } else if ((result > - 0.05) && (result < 0)) {
+            // assume we will be better to reach 5%
+            value = recentValue * (1 + 0.05 + result);
+
+        } else if (result < -0.05) {
+            // assume little improve
+            value = recentValue * (1 + result / 2);
+        } else {
+            value = recentValue;
+        }
+
+        item.put("key", formatter.print(month));
+        item.put("value", value);
+
+        return item;
+    }
+
+    private void createForecastBar() {
+        barMonthlyForecast.setLegendPosition("ne");
+        barMonthlyForecast.setExtender("barMonthlyForecast");
+
+        Axis xAxis = barMonthlyForecast.getAxis(AxisType.X);
+        Axis yAxis = barMonthlyForecast.getAxis(AxisType.Y);
+
+        xAxis.setTickAngle(-50);
+        xAxis.setTickFormat("%y-%m");
+
+        long interval = 1000 * 60 * 60 * 24 * 30 * 3;
+        xAxis.setTickInterval(String.valueOf(interval));
+        barMonthlyForecast.setBarWidth(11);
+        barMonthlyForecast.setBarMargin(6);
+        barMonthlyForecast.getAxes().put(AxisType.X, xAxis);
+
+        String label = revenueStr;
+        drawBar(barMonthlyForecast, label, forecastRevenue);
+
+        label = profitStr;
+        drawBar(barMonthlyForecast, label, forecastProfit);
+    }
+
+    private List<Map<String, Object>> queryForecastRevenue() {
+        sql = forecastMonthRevenue;
+        logger.info("Get 24 months revenue by month");
+        forecastRevenue = queryMonthDate(sql, sqlParams, 24, fcStartMonth);
+
+        return forecastRevenue;
+    }
+
+    private List<Map<String, Object>> queryForecastMaint() {
+        sql = forecastMonthMaint;
+
+        logger.info("Get 24 months maint cost by month");
+        forecastMaint = queryMonthDate(sql, sqlParams, 24, fcStartMonth);
+
+        return forecastMaint;
+    }
+
+    private List<Map<String, Object>> queryForecastDep() {
+        sql = depMonth;
+        logger.info("Get 24 months by month");
+        monthDep = queryMonthDepDate(sql, sqlParams, 24, fcStartMonth);
+
+        return monthDep;
+    }
+
+    private List<Map<String, Object>> queryMonthDepDate(String sql, HashMap<String, Object> sqlParams, int months, DateTime startMonth) {
+        logger.info("Get dep by month, sql: {}, sqlParams: {}", sql, sqlParams);
+        List<Map<String, Object>> li = NativeSqlUtil.queryForList(sql, sqlParams);
+        if (li.size() == 0) {
+            li = calcMonthlyDep(0.0, months, startMonth);
+        } else {
+            li = calcMonthlyDep((double) li.get(0).get("value"), months, startMonth);
+        }
+        return li;
+    }
+
+    private List<Map<String, Object>> queryMonthDate(String sql, HashMap<String, Object> sqlParams, int months, DateTime startMonth) {
+        logger.info("Get data by month, sql: {}, sqlParams: {}", sql, sqlParams);
+        List<Map<String, Object>> li = NativeSqlUtil.queryForList(sql, sqlParams);
+        return formatMonthlyData(li, months, startMonth);
+    }
+
+    public String getProfitForecast() { return profitForecast; }
+
+    public String getSelectedYear() {
+        logger.debug("get selectdYear: {}", selectedYear);
+
+        return selectedYear;
+    }
+
+    public void setSelectedYear(String selectedYear) {
+        logger.debug("set selectedYear: {}", selectedYear);
+        this.selectedYear = selectedYear;
+    }
+
+    public BarChartModel getBarMonthlyForecast() { return barMonthlyForecast; }
+
+    private List<Map<String, Object>> calcMonthlyDep(double annualDepValue, int months, DateTime startMonth) {
+        List<Map<String, Object>> li = new ArrayList<>();
+        double value = annualDepValue / 12;
+
+        // calc monthly dep value
+        DateTime targetMonth = startMonth;
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM");
+        for (int index = 0; index < months; index++) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("key", formatter.print(targetMonth));
+            item.put("value", value);
+            li.add(item);
+            targetMonth = targetMonth.plusMonths(1);
+        }
+
+        return li;
+    }
+
+    private List<Map<String, Object>> formatMonthlyData(List<Map<String, Object>> result, int size, DateTime startMonth) {
+        List<Map<String, Object>> li = new ArrayList<>();
+
+        if (size <= 0) return li;
+
+        // init expected result list
+        DateTime targetMonth = startMonth;
+        logger.debug("startMonth: {}.", startMonth);
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM");
+        for (int index = 0; index < size; index++) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("key", formatter.print(targetMonth));
+            item.put("value", 0.0);
+            li.add(item);
+            targetMonth = targetMonth.plusMonths(1);
+        }
+
+        logger.debug("result size: {}", result.size());
+        logger.debug("print input result: {}", result);
+        for (Map<String, Object> item : li) {
+
+            if (result.size() == 0) {
+                break; // all items in result list have been removed, therefore, finish for loop
+            }
+
+            HashMap<String, Object> node = (HashMap<String, Object>) result.get(0);
+
+            while ((node.get("key") == null)  // remove node when the value of "key" is null
+                    // remove node when it is earlier than expected item
+                    || (formatter.parseDateTime(node.get("key").toString()).compareTo(
+                    formatter.parseDateTime(item.get("key").toString())) < 0)) {
+                result.remove(0);
+                node = (HashMap<String, Object>) result.get(0);
+            }
+
+            node = (HashMap<String, Object>) result.get(0);
+
+            if (node.get("key").toString().equals(item.get("key").toString())) {
+                if (node.get("value") != null) {
+                    item.put("value", node.get("value"));
+                }
+                result.remove(0); // since value of node has been put to item, therefore rmove head of list
+            }
+        }
+        return li;
+    }
+
+    private List<Map<String, Object>> calcProfit(List<Map<String, Object>> revenue,
+                                                 List<Map<String, Object>> dep,
+                                                 List<Map<String, Object>> maintenance
+                                                 ) {
+        // Calcuate profile = revenue - depreciation - maintenance cost
+        int index = 0;
+        List<Map<String, Object>> profit = new ArrayList<>();
+
+        for (Map<String, Object> item : revenue) {
+            String key = item.get("key").toString();
+
+            HashMap<String, Object> node = new HashMap<>();
+
+            double value = (Double) item.get("value");
+            if ((value > -0.00001) && (value < 0.00001)) {
+                // revenue equals zero, it should be data input error, therefore, profit will be zero too
+                node.put("value", 0.0);
+            } else {
+
+                // revenue did not equal to zero, let us calculate profit
+                if (dep.get(index).get("key") == null) {
+                    logger.debug("Cannot get key: " + key + "in deprecate sql query.");
+                } else {
+                    value = value - (Double) dep.get(index).get("value");
+                }
+
+                if (maintenance.get(index).get("key") == null) {
+                    logger.debug("Cannot get key: " + key + "in maintenance sql query.");
+                } else {
+                    value = value - (Double) maintenance.get(index).get("value");
+                }
+                node.put("value", value);
+            }
+
+            node.put("key", key);
+            profit.add(node);
+            index++;
+        }
+
+        return profit;
+    }
+
+    private List<Map<String, Object>> prepareData(String sql, HashMap<String, Object> sqlParams) {
+        logger.debug(sql);
+
+        List<Map<String, Object>> result = NativeSqlUtil.queryForList(sql, sqlParams);
+
+        if (result.size() == 0) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("key", 1);
+            item.put("value", 0.1);
+            result.add(item);
+        }
+        else {
+            for (Map<String, Object> item : result) {
+                checkNull(item, "Double");
+            }
+        }
+
+
+        return result;
+    }
+
+    private void drawBar(BarChartModel barChart, String label, List<Map<String, Object>> result) {
+        ChartSeries cs = new ChartSeries();
+        cs.setLabel(label);
+
+        for (Map<String, Object> item : result) {
+            cs.set(item.get("key").toString(), (Double) item.get("value"));
+        }
+
+        barChart.addSeries(cs);
+    }
+
+    private void checkNull(Map<String, Object> item, String targetType) {
+        if (item.get("value") == null) {
+
+            // add targetType, if needed.
+            // convert null to associated empty value for drawing
+            switch (targetType) {
+
+                case "Double":
+                    item.put("value", 0.1);
+                default:
+                    ;
+            }
+        }
+    }
+}
