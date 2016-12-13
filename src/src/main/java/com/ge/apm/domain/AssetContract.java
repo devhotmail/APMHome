b1ddd50fa647a56738de606b336bde3e631740d3
wujb <wujb@42624c65-7419-e447-9ad7-a502218ba35c>
2016-12-13 15:30:28 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2740 42624c65-7419-e447-9ad7-a502218ba35c

---
+++ src/src/main/java/com/ge/apm/domain/AssetContract.java
@@ -0,0 +1,173 @@
+/*
+ */
+package com.ge.apm.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author 212547631
+ */
+@Entity
+@Table(name = "asset_contract")
+@NamedQueries({
+    @NamedQuery(name = "AssetContract.findAll", query = "SELECT a FROM AssetContract a")})
+public class AssetContract implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "site_id")
+    private int siteId;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "asset_id")
+    private int assetId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 64)
+    @Column(name = "name")
+    private String name;
+    @Column(name = "file_id")
+    private Integer fileId;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "start_date")
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "end_date")
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "contract_amount")
+    private double contractAmount;
+
+    public AssetContract() {
+    }
+
+    public AssetContract(Integer id) {
+        this.id = id;
+    }
+
+    public AssetContract(Integer id, int siteId, int assetId, String name, Date startDate, Date endDate, double contractAmount) {
+        this.id = id;
+        this.siteId = siteId;
+        this.assetId = assetId;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.contractAmount = contractAmount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(int siteId) {
+        this.siteId = siteId;
+    }
+
+    public int getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(int assetId) {
+        this.assetId = assetId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getContractAmount() {
+        return contractAmount;
+    }
+
+    public void setContractAmount(double contractAmount) {
+        this.contractAmount = contractAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AssetContract)) {
+            return false;
+        }
+        AssetContract other = (AssetContract) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ge.apm.domain.AssetContract[ id=" + id + " ]";
+    }
+    
+}
