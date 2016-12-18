52d29e304c0f13fd3ded418cef9306c81d642940
shenqt <shenqt@42624c65-7419-e447-9ad7-a502218ba35c>
2016-12-18 08:45:15 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@2880 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/dao/InspectionOrderRepository.java
+++ src/src/main/java/com/ge/apm/dao/InspectionOrderRepository.java
@@ -7,6 +7,7 @@ import webapp.framework.dao.GenericRepository;
 
 public interface InspectionOrderRepository extends GenericRepository<InspectionOrder> {
 
-     @Query("select  distinct o from InspectionOrder o,InspectionOrderDetail d where o.id=d.orderId and d.assetId=?1 and o.orderType=?2 and o.startTime>current_date group by o.id order by o.startTime ")
+//     @Query("select  distinct o from InspectionOrder o,InspectionOrderDetail d where o.id=d.orderId and d.assetId=?1 and o.orderType=?2 and o.startTime>current_date group by o.id order by o.startTime ")
+     @Query("select  distinct o from InspectionOrder o,InspectionOrderDetail d where o.id=d.orderId and d.assetId=?1 and o.orderType=?2 and o.isFinished=false  group by o.id order by o.startTime ")
     public List<InspectionOrder> getRecentlyInspectionOrder(Integer assetId,Integer orderType);
 }
