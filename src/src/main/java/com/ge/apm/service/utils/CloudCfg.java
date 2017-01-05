968093ffe2bf670b121b5daad2b9bbbeff7f65f8
chenyinghua <chenyinghua@42624c65-7419-e447-9ad7-a502218ba35c>
2017-01-05 04:51:45 +0000

    pom.xml: updated maven-jetty-plugin to be compatible with java 8 updated javax.faces artifact to be compatible with java 8 imported javaslang, rxjava for microservices dev
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@3108 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/service/utils/CloudCfg.java
+++ src/src/main/java/com/ge/apm/service/utils/CloudCfg.java
@@ -2,15 +2,14 @@ package com.ge.apm.service.utils;
 
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
-import com.google.common.collect.FluentIterable;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import javaslang.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
-import org.springframework.util.SystemPropertyUtils;
 
-import java.io.FileNotFoundException;
+import java.util.stream.StreamSupport;
 
 public class CloudCfg {
     private final static Logger log = LoggerFactory.getLogger(CloudCfg.class);
@@ -18,7 +17,7 @@ public class CloudCfg {
     private String jdbcUserName;
     private String jdbcPassword;
 
-    public CloudCfg() throws FileNotFoundException {
+    public CloudCfg() {
         log.info("VCAP_SERVICES: {}", System.getenv("VCAP_SERVICES"));
         if (Strings.isNullOrEmpty(System.getenv("VCAP_SERVICES"))) {
             loadCfgFromProperties();
@@ -29,13 +28,13 @@ public class CloudCfg {
 
     private void loadCfgFromSysEnv() {
         Config conf = ConfigFactory.parseString(System.getenv("VCAP_SERVICES"));
-        jdbcUrl = FluentIterable.from(Splitter.on("?").limit(2).split(conf.getConfigList("postgres").get(0).getString("credentials.jdbc_uri"))).first().or("");
+        jdbcUrl = StreamSupport.stream(Splitter.on("?").limit(2).split(conf.getConfigList("postgres").get(0).getString("credentials.jdbc_uri")).spliterator(), false).findFirst().orElse("");
         jdbcUserName = conf.getConfigList("postgres").get(0).getString("credentials.username");
         jdbcPassword = conf.getConfigList("postgres").get(0).getString("credentials.password");
     }
 
-    private void loadCfgFromProperties() throws FileNotFoundException {
-        Config conf = ConfigFactory.parseFile(ResourceUtils.getFile("classpath:database.properties"));
+    private void loadCfgFromProperties() {
+        Config conf = Try.of(() -> ConfigFactory.parseFile(ResourceUtils.getFile("classpath:database.properties"))).get();
         jdbcUrl = conf.getString("jdbc.url");
         jdbcUserName = conf.getString("jdbc.user");
         jdbcPassword = conf.getString("jdbc.password");
