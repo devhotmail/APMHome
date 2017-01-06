dec68408cf6b699eb8ebe25e3ac6494bbac3ffdc
jessezhang <jessezhang@42624c65-7419-e447-9ad7-a502218ba35c>
2017-01-06 08:51:04 +0000

    Remove about.html from source repo to avoid false commit
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@3122 42624c65-7419-e447-9ad7-a502218ba35c

--- src/about.sh
+++ src/about.sh
@@ -1,10 +1,24 @@
+#!/bin/bash
+# enable version control in /about.html
+echo -e "<html>  
+   	<head>   
+       	<title>GE APM</title>  
+       	<meta charset="utf-8">  
+   	</head>  
+   	<body>  
+		<div>GE Healthcare (China) APM</div>
+		<div>Version: \$version</div>
+		<div>Build: \$build</div>  
+   	</body>  
+</html>" > ./src/main/webapp/about.html
+
 version=1.0
 build=`svn log --limit 1 |awk 'NR==2{print}' |awk '{print $1}'`
 build=${build#*r}
 
-sed -i 's/$version/'"$version"'/' ./target/geapm-*/about.html 
-sed -i 's/$build/'"$build"'/' ./target/geapm-*/about.html
+echo "Version: $version"
+echo "Build: $build"
 
-
-
+sed -i 's/$version/'"$version"'/'  ./src/main/webapp/about.html
+sed -i 's/$build/'"$build"'/' ./src/main/webapp/about.html
 
