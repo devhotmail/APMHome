bbc1ffb05ebeef94b74508e84b46a06df994d69f
lindai <lindai@42624c65-7419-e447-9ad7-a502218ba35c>
2017-01-16 10:29:45 +0000

    增加微信菜单创建功能
    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@3176 42624c65-7419-e447-9ad7-a502218ba35c

---
+++ src/src/main/java/com/ge/apm/view/wechat/config/MenuConfig.java
@@ -0,0 +1,71 @@
+package com.ge.apm.view.wechat.config;
+
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.bean.menu.WxMenu;
+import me.chanjar.weixin.common.bean.menu.WxMenuButton;
+import me.chanjar.weixin.mp.api.WxMpService;
+
+public class MenuConfig {
+
+    /**
+     * 定义菜单结构
+     *
+     * @return
+     */
+    public static WxMenu getMenu(WxMpService wxMpService) {
+
+        WxMenu menu = new WxMenu();
+        WxMenuButton button1 = new WxMenuButton();
+        button1.setName("设备信息");
+//        button1.setUrl(wxMpService.oauth2buildAuthorizationUrl("http://danny.ittun.com/weixin-java-tools-springmvc/authurl", 
+//                WxConsts.OAUTH2_SCOPE_USER_INFO, ""));
+        WxMenuButton button11 = new WxMenuButton();
+        button11.setType(WxConsts.BUTTON_VIEW);
+        button11.setName("查看设备信息");
+        button11.setUrl("http://danny.ittun.com/geapm");
+        WxMenuButton button12 = new WxMenuButton();
+        button12.setType(WxConsts.BUTTON_VIEW);
+        button12.setName("新增设备");
+        button12.setUrl("http://danny.ittun.com/geapm");
+        button1.getSubButtons().add(button11);
+        button1.getSubButtons().add(button12);
+
+        WxMenuButton button2 = new WxMenuButton();
+        button2.setName("维修流程");
+        WxMenuButton button21 = new WxMenuButton();
+        button21.setType(WxConsts.BUTTON_VIEW);
+        button21.setName("新增报修");
+        button21.setUrl("http://danny.ittun.com/geapm");
+        WxMenuButton button22 = new WxMenuButton();
+        button22.setType(WxConsts.BUTTON_VIEW);
+        button22.setName("报修处理进度");
+        button22.setUrl("http://danny.ittun.com/geapm");
+        button2.getSubButtons().add(button21);
+        button2.getSubButtons().add(button22);
+
+        WxMenuButton button3 = new WxMenuButton();
+        button3.setName("我的帐号");
+        WxMenuButton button31 = new WxMenuButton();
+        button31.setType(WxConsts.BUTTON_VIEW);
+        button31.setName("APM帐号信息");
+        button31.setUrl("http://danny.ittun.com/geapm");
+        WxMenuButton button32 = new WxMenuButton();
+        button32.setType(WxConsts.BUTTON_VIEW);
+        button32.setName("APM重置密码");
+        button32.setUrl("http://danny.ittun.com/geapm");
+        WxMenuButton button33 = new WxMenuButton();
+        button33.setType(WxConsts.BUTTON_VIEW);
+        button33.setName("APM帐号绑定");
+        button33.setUrl("http://danny.ittun.com/geapm");
+        button3.getSubButtons().add(button31);
+        button3.getSubButtons().add(button32);
+        button3.getSubButtons().add(button33);
+
+        menu.getButtons().add(button1);
+        menu.getButtons().add(button2);
+        menu.getButtons().add(button3);
+
+        return menu;
+    }
+
+}
