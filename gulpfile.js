1ae278531e2ae5e755d81454ae3bf565d2471a58
Garry Yao <yaojun85@gmail.com>
2017-01-17 16:30:29 +0800

    Revert unexpected changes to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
+const path = require('path');
 const autoprefixer = require('gulp-autoprefixer');
+const stylemod = require('gulp-style-modules');
 const sass = require('gulp-sass');
 const gulp = require('gulp');
 const cssmin = require('gulp-cssmin');
 const importOnce = require('node-sass-import-once');
 const src = 'src/main/webapp/resources/sass/*.scss';
+const src_comp = 'src/main/webapp/resources/elements/**/*.scss';
 const dest = 'src/main/webapp/resources/css/';
 
 // -------------------------------------
 //   Task: Compile: Sass
 // -------------------------------------
 
-gulp.task('sass:compile', function() {
+gulp.task('sass:compile:css', function() {
   return gulp.src([
     src,
   ])
@@ -36,12 +39,49 @@ gulp.task('sass:compile', function() {
   .pipe(gulp.dest(dest));
 });
 
+gulp.task('sass:compile:module', function() {
+
+  var fileName = function(file) {
+    return path.basename(file.path, path.extname(file.path));
+  };
+
+  return gulp.src([
+    src_comp
+  ])
+  .pipe(sass({
+    style: 'compressed',
+    importer: importOnce,
+    importOnce: {
+      index: true,
+      css: true,
+      bower: true
+    }
+  })
+  .on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(stylemod({
+    // All files will be named 'styles.html'
+    filename: function(file) {
+      var name = fileName(file) + '-styles';
+      return name;
+    },
+    // Use '-css' suffix instead of '-styles' for module ids
+    moduleId: function(file) {
+      return fileName(file) + '-styles';
+    }
+  }))
+  .pipe(gulp.dest(function(file) {
+    return file.base;
+  }));
+});
+
 // -------------------------------------
 //   Task: Watch: Sass
 // -------------------------------------
 
 gulp.task('sass:watch', function() {
-  gulp.watch(src, ['sass:compile']);
+  gulp.watch(src, ['sass:compile:css']);
+  gulp.watch(src_comp, ['sass:compile:module']);
 });
 
-gulp.task('default', ['sass:compile', 'sass:watch']);
+gulp.task('default', ['sass:compile:css', 'sass:compile:module','sass:watch']);
