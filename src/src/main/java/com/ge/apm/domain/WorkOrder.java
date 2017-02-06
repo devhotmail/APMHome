ec24945e0891f9507af2f4c0e298254326a260ff
gaolei <gaolei@42624c65-7419-e447-9ad7-a502218ba35c>
2017-02-06 03:13:57 +0000

    git-svn-id: https://123.57.29.132:8443/svn/GEHCIT/APS@3297 42624c65-7419-e447-9ad7-a502218ba35c

--- src/src/main/java/com/ge/apm/domain/WorkOrder.java
+++ src/src/main/java/com/ge/apm/domain/WorkOrder.java
@@ -2,19 +2,13 @@
  */
 package com.ge.apm.domain;
 
-import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
