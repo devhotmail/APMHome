97db7d6e9ed52b680e2b583ba19a89a07494fc0a
212579464 <qingtao.shen@ge.com>
2017-02-17 15:24:11 +0800

    added page for wechat asset create

--- src/src/main/java/com/ge/apm/service/asset/AttachmentFileService.java
+++ src/src/main/java/com/ge/apm/service/asset/AttachmentFileService.java
@@ -6,6 +6,9 @@
 package com.ge.apm.service.asset;
 
 import com.ge.apm.dao.FileUploadDao;
+import com.ge.apm.service.wechat.CoreService;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -15,6 +18,7 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
@@ -35,17 +39,21 @@ public class AttachmentFileService {
         Integer id = saveFiletoDB(file);
         return id;
     }
+    
     public String getFileName(UploadedFile file) {
+        return getFileName(file.getFileName());
+    }
+
+    public String getFileName(String rowName){
         String fileName = "";
         try {
-            fileName = new String(file.getFileName().getBytes(), "utf-8");
+            fileName = new String(rowName.getBytes(), "utf-8");
         } catch (UnsupportedEncodingException ex) {
             WebUtil.addErrorMessage(WebUtil.getMessage("fileTransFail"));
             Logger.getLogger(AttachmentFileService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return fileName;
+         return fileName;
     }
-
     
     public String getFileNameById(Integer fileId) {
         try {
@@ -87,6 +95,38 @@ public class AttachmentFileService {
     public boolean deleteAttachment(Integer fileId) {
         fileUploaddao.deleteUploadFile(fileId);
         return true;
+    }
+
+    public Integer uploadFile(Part file) {
+        Integer returnId = 0;
+        try {
+            returnId = fileUploaddao.saveUploadFile(file.getInputStream(), Integer.valueOf(String.valueOf(file.getSize())), file.getSubmittedFileName());
+        } catch (SQLException | IOException ex) {
+            WebUtil.addErrorMessage(WebUtil.getMessage("fileTransFail"));
+            Logger.getLogger(AttachmentFileService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return returnId;
+    }
+    
+    public Integer uploadFile(File file) {
+        Integer returnId = 0;
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+            returnId = fileUploaddao.saveUploadFile(is, Integer.valueOf(String.valueOf(file.length())), file.getName());
+        } catch (SQLException | IOException ex) {
+            WebUtil.addErrorMessage(WebUtil.getMessage("fileTransFail"));
+            Logger.getLogger(AttachmentFileService.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            if(is != null) {
+                try{
+                    is.close();
+                }catch (IOException ex) {
+                    Logger.getLogger(CoreService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return returnId;
     }
     
 
