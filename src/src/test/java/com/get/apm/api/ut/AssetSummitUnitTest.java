95b4fabc09f9ab91a9b872d78dff477361caa548
Ivar <ivar.chen@ge.com>
2017-02-23 16:00:07 +0800

    updated to log4js updated AssetSummitUnitTest, replace Random to ThreadLocalRandom

--- src/src/test/java/com/get/apm/api/ut/AssetSummitUnitTest.java
+++ src/src/test/java/com/get/apm/api/ut/AssetSummitUnitTest.java
@@ -6,7 +6,6 @@ import com.github.davidmoten.rx.jdbc.Database;
 import javaslang.Tuple;
 import javaslang.Tuple6;
 import javaslang.collection.Stream;
-import org.apache.commons.lang.math.RandomUtils;
 import org.apache.ibatis.jdbc.SQL;
 import org.junit.After;
 import org.junit.Before;
@@ -19,10 +18,11 @@ import rx.Observable;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class AssetSummitUnitTest {
   private final Logger log = LoggerFactory.getLogger(DynamicSqlTest.class);
-  private final List<LocalDate> dates = Stream.iterate(LocalDate.now().minusYears(3), now -> now.plusDays(1)).takeUntil(date -> date.isAfter(LocalDate.now())).toJavaList();
+  private final List<LocalDate> dates = Stream.iterate(LocalDate.now().minusYears(3), d -> d.plusDays(1)).takeUntil(date -> date.isAfter(LocalDate.now())).toJavaList();
   private final String sql = new SQL().INSERT_INTO("asset_summit")
     .VALUES("site_id", ":site_id")
     .VALUES("hospital_id", ":hospital_id")
@@ -58,7 +58,7 @@ public class AssetSummitUnitTest {
     dataSource.addFilters("slf4j");
     dataSource.setValidationQuery("SELECT 1");
     db = Database.from(new ConnectionProviderFromDataSource(dataSource));
-    assets = db.select(new SQL().SELECT("site_id", "hospital_id", "id", "asset_group", "asset_dept_id").FROM("asset_info").ORDER_BY("id").toString()).getAs(Integer.class, Integer.class, Integer.class, Integer.class, Integer.class).map(t -> Tuple.of(t._1(), t._2(), t._3(), t._4(), t._5(), RandomUtils.nextInt(5) + 1)).cache();
+    assets = db.select(new SQL().SELECT("site_id", "hospital_id", "id", "asset_group", "asset_dept_id").FROM("asset_info").ORDER_BY("id").toString()).getAs(Integer.class, Integer.class, Integer.class, Integer.class, Integer.class).map(t -> Tuple.of(t._1(), t._2(), t._3(), t._4(), t._5(), ThreadLocalRandom.current().nextInt(1, 5))).cache();
     db.update(new SQL().DELETE_FROM("asset_summit").toString()).count().toBlocking().single();
   }
 
@@ -81,18 +81,19 @@ public class AssetSummitUnitTest {
         .parameter("asset_group", t._4)
         .parameter("dept_id", t._5)
         .parameter("supplier_id", t._6)
-        .parameter("revenue", RandomUtils.nextInt(999))
-        .parameter("maintenance_cost", RandomUtils.nextInt(99))
-        .parameter("deprecation_cost", RandomUtils.nextInt(99))
-        .parameter("inject_count", RandomUtils.nextInt(9))
-        .parameter("expose_count", RandomUtils.nextInt(9))
-        .parameter("film_count", RandomUtils.nextInt(9))
-        .parameter("exam_count", RandomUtils.nextInt(9))
-        .parameter("exam_duration", RandomUtils.nextInt(999))
-        .parameter("down_time", RandomUtils.nextInt(999))
-        .parameter("work_order_count", RandomUtils.nextInt(1))
-        .parameter("rating", RandomUtils.nextInt(999))
-        .parameter("created", t._7).parameter("last_modified", t._7)
+        .parameter("revenue", ThreadLocalRandom.current().nextInt(1, 999))
+        .parameter("maintenance_cost", ThreadLocalRandom.current().nextInt(1, 500))
+        .parameter("deprecation_cost", ThreadLocalRandom.current().nextInt(1, 500))
+        .parameter("inject_count", ThreadLocalRandom.current().nextInt(1, 200))
+        .parameter("expose_count", ThreadLocalRandom.current().nextInt(0, 200))
+        .parameter("film_count", ThreadLocalRandom.current().nextInt(0, 100))
+        .parameter("exam_count", ThreadLocalRandom.current().nextInt(0, 100))
+        .parameter("exam_duration", ThreadLocalRandom.current().nextInt(1, 80000))
+        .parameter("down_time", ThreadLocalRandom.current().nextInt(1, 500))
+        .parameter("work_order_count", ThreadLocalRandom.current().nextInt(0, 1))
+        .parameter("rating", ThreadLocalRandom.current().nextInt(1, 999))
+        .parameter("created", t._7)
+        .parameter("last_modified", t._7)
         .returnGeneratedKeys()
         .getAs(Integer.class)
         .toBlocking()
