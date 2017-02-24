5a17ca83b95686b8d0551b55d52be1c5a5493d2f
Ivar <ivar.chen@ge.com>
2017-02-24 17:13:08 +0800

    assets api update

--- src/src/main/java/com/ge/apm/service/api/AssetsService.java
+++ src/src/main/java/com/ge/apm/service/api/AssetsService.java
@@ -1,12 +1,20 @@
 package com.ge.apm.service.api;
 
+import com.ge.apm.service.utils.CNY;
 import com.github.davidmoten.rx.jdbc.ConnectionProvider;
 import com.github.davidmoten.rx.jdbc.Database;
+import javaslang.Tuple;
+import javaslang.Tuple6;
+import org.apache.ibatis.jdbc.SQL;
+import org.javamoney.moneta.Money;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import rx.Observable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.time.LocalDate;
 
 public class AssetsService {
   private static final Logger log = LoggerFactory.getLogger(AssetsService.class);
@@ -20,5 +28,18 @@ public class AssetsService {
     db = Database.from(connectionProvider);
   }
 
-
+  @Cacheable(cacheNames = "springCache", key = "'assetsService.findAssets.'+#siteId+'.'+#hospitalId")
+  public Observable<Tuple6<Integer, String, Integer, Integer, Money, LocalDate>> findAssets(int siteId, int hospitalId, String orderBy) {
+    return db.select(new SQL()
+      .SELECT("id", "name", "asset_group", "supplier_id", "purchase_price", "arrive_date")
+      .FROM("asset_info")
+      .WHERE("site_id = :site_id")
+      .WHERE("hospital_id = :hospital_id")
+      .ORDER_BY("id")
+      .toString())
+      .parameter("site_id", 1)
+      .parameter("hospital_id", 1)
+      .getAs(Integer.class, String.class, Integer.class, Integer.class, Double.class, LocalDate.class)
+      .map(t -> Tuple.of(t._1(), t._2(), t._3(), t._4(), CNY.money(t._5()), t._6()));
+  }
 }
--- src/src/main/java/com/ge/apm/web/AssetsApi.java
+++ src/src/main/java/com/ge/apm/web/AssetsApi.java
@@ -1,13 +1,21 @@
 package com.ge.apm.web;
 
 
+import com.ge.apm.domain.UserAccount;
 import com.ge.apm.service.api.CommonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import webapp.framework.web.service.UserContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/assets")
@@ -16,5 +24,20 @@ public class AssetsApi {
   private static final Logger log = LoggerFactory.getLogger(AssetsApi.class);
   @Autowired
   private CommonService commonService;
+
+  @RequestMapping(method = RequestMethod.GET)
+  @ResponseBody
+  public ResponseEntity<Map<String, Object>> handleRequest(HttpServletRequest request,
+                                                           @Pattern(regexp = "type|dept|supplier|revenue|yoa") @RequestParam(value = "orderby", required = false) String orderBy,
+                                                           @Min(1) @Max(50) @RequestParam(value = "limit", required = false, defaultValue = "20") Integer limit,
+                                                           @Min(0) @RequestParam(value = "start", required = false, defaultValue = "0") Integer start) {
+    log.info("orderBy:{}, limit:{}, start:{}", orderBy, limit, start);
+    UserAccount user = UserContext.getCurrentLoginUser();
+    Map<Integer, String> groups = commonService.findFields(user.getSiteId(), "assetGroup");
+    Map<Integer, String> depts = commonService.findDepts(user.getSiteId(), user.getHospitalId());
+
+
+    return null;
+  }
 
 }
