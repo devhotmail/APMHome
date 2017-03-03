11b0f2998d4a76de238d41c85e9dd621808dbf85
Ivar <ivar.chen@ge.com>
2017-03-03 11:29:35 +0800

    fixed bug of commonService added messageApi

--- src/src/main/java/com/ge/apm/service/api/CommonService.java
+++ src/src/main/java/com/ge/apm/service/api/CommonService.java
@@ -40,45 +40,43 @@ public class CommonService {
 
   @Cacheable(cacheNames = "springCache", key = "'commonService.findAllMessages'")
   public Iterable<Tuple7<Integer, String, String, String, String, String, Integer>> findAllMessages() {
-    return db.select(new SQL().SELECT("id", "msg_type", "msg_key", "value_zh", "value_en", "value_tw", "site_id").FROM("i18n_message").toString()).getAs(Integer.class, String.class, String.class, String.class, String.class, String.class, Integer.class).map(t -> Tuple.of(t._1(), t._2(), t._3(), t._4(), t._5(), t._6(), t._7())).sorted((l, r) -> l._1 - r._1).toBlocking().toIterable();
+    return db.select(new SQL().SELECT("id", "msg_type", "msg_key", "value_zh", "value_en", "value_tw", "site_id").FROM("i18n_message").toString())
+      .getAs(Integer.class, String.class, String.class, String.class, String.class, String.class, Integer.class)
+      .map(t -> Tuple.of(t._1(), t._2(), t._3(), t._4(), t._5(), t._6(), t._7())).sorted((l, r) -> l._1 - r._1)
+      .cache().toBlocking().toIterable();
   }
 
   @Cacheable(cacheNames = "springCache", key = "'commonService.findFields.'+#siteId+'.'+#type")
   public Map<String, String> findFields(int siteId, String type) {
     return Observable.from(Option.of(DbMessageSource.getMessageCache(siteId)).filter(map -> map.entrySet().stream().anyMatch(entry -> entry.getValue().getMsgType().equalsIgnoreCase(type))).getOrElse(DbMessageSource.getMessageCache(-1)).entrySet())
       .filter(entry -> entry.getValue().getMsgType().equalsIgnoreCase(type))
-      .map(Map.Entry::getValue).toMap(I18nMessage::getMsgKey, I18nMessage::getValueZh).toBlocking().single();
+      .map(Map.Entry::getValue).toMap(I18nMessage::getMsgKey, I18nMessage::getValueZh)
+      .cache().toBlocking().single();
   }
 
   @Cacheable(cacheNames = "springCache", key = "'commonService.findHospitals'")
   public Map<Integer, Tuple3<Integer, String, String>> findHospitals() {
-    return db
-      .select(new SQL().SELECT("id", "name", "alias_name").FROM("site_info").toString())
+    return db.select(new SQL().SELECT("id", "name", "alias_name").FROM("site_info").toString())
       .getAs(Integer.class, String.class, String.class)
       .toMap(com.github.davidmoten.rx.jdbc.tuple.Tuple3::_1, t -> Tuple.of(t._1(), Option.of(t._2()).getOrElse(""), Option.of(t._3()).getOrElse("")))
-      .toBlocking()
-      .single();
+      .cache().toBlocking().single();
   }
 
   @Cacheable(cacheNames = "springCache", key = "'commonService.findDepts.'+#siteId+'.'+#hospitalId")
   public Map<Integer, String> findDepts(int siteId, int hospitalId) {
-    return db
-      .select(new SQL().SELECT_DISTINCT("clinical_dept_id", "clinical_dept_name").FROM("asset_info").WHERE("site_id = :site_id ").WHERE("hospital_id = :hospital_id").toString())
+    return db.select(new SQL().SELECT_DISTINCT("clinical_dept_id", "clinical_dept_name").FROM("asset_info").WHERE("site_id = :site_id ").WHERE("hospital_id = :hospital_id").toString())
       .parameter("site_id", siteId).parameter("hospital_id", hospitalId)
       .getAs(Integer.class, String.class)
       .toMap(Tuple2::_1, tuple -> Option.of(tuple._2()).getOrElse(""))
-      .toBlocking()
-      .single();
+      .cache().toBlocking().single();
   }
 
   @Cacheable(cacheNames = "springCache", key = "'commonService.findSuppliers.'+#siteId")
   public Map<Integer, String> findSuppliers(int siteId) {
-    return db
-      .select(new SQL().SELECT("id", "name").FROM("supplier").WHERE("site_id = :site_id").toString())
+    return db.select(new SQL().SELECT("id", "name").FROM("supplier").WHERE("site_id = :site_id").toString())
       .parameter("site_id", siteId).getAs(Integer.class, String.class)
       .toMap(Tuple2::_1, Tuple2::_2)
-      .toBlocking()
-      .single();
+      .cache().toBlocking().single();
   }
 
   public ResponseEntity.BodyBuilder headerUserProfile(ResponseEntity.BodyBuilder builder, UserAccount user, Map<Integer, Tuple3<Integer, String, String>> hospitals) {
