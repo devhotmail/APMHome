7efc2eab30ad2b6d081ec82ea1068f2eefc16009
Gao <212593079@logon.ds.ge.com>
2017-03-07 11:07:33 +0800

    delete unuse code

--- src/bin/sql/insert-apm-data/manifest.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-cat ../create_tables.sql > create_tables.sql
-cat ../init_data.sql ../i18n_message.sql ../test_data.sql > test_data.sql
-#cf login -a https://api.system.aws-jp01-pr.ice.predix.io
-cf push -f manifest_.yml
--- src/bin/src/main/resources/codegen/dao.ftl
+++
@@ -1,8 +0,0 @@
-package ${basePackage}.dao;
-
-import ${basePackage}.domain.${entityName};
-import webapp.framework.dao.GenericRepository;
-
-public interface ${entityName}Repository extends GenericRepository<${entityName}> {
-
-}
