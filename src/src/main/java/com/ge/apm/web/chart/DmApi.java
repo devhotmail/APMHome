fd5052fd4e130c750204ee0bba80985b8a66fa89
Ivar <ivar.chen@ge.com>
2017-03-17 14:29:03 +0800

    Merge branch 'Obserable' of /Users/ivar/Documents/code/java/dew with conflicts.

--- src/src/main/java/com/ge/apm/web/chart/DmApi.java
+++ src/src/main/java/com/ge/apm/web/chart/DmApi.java
@@ -6,8 +6,10 @@ import com.ge.apm.service.api.DmService;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.math.Stats;
 import com.google.common.primitives.Ints;
-import javaslang.*;
-import javaslang.collection.Seq;
+import javaslang.Tuple;
+import javaslang.Tuple3;
+import javaslang.Tuple4;
+import javaslang.Tuple5;
 import javaslang.control.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import rx.Observable;
+import rx.observables.MathObservable;
 import webapp.framework.web.service.UserContext;
 
 import javax.servlet.http.HttpServletRequest;
@@ -44,12 +47,14 @@ public class DmApi {
     log.info("groupby:{}", groupBy);
     UserAccount user = UserContext.getCurrentLoginUser();
     Map<Integer, String> groups = Observable.from(commonService.findFields(user.getSiteId(), "dept".equals(groupBy) ? "clinicalDeptId" : "assetGroup").entrySet()).filter(e -> Option.of(Ints.tryParse(e.getKey())).isDefined()).toMap(e -> Ints.tryParse(e.getKey()), Map.Entry::getValue).toBlocking().single();
-    return serialize(request, groupBy, mapItems(groups, averageUsage(usagePredict(dmService.findAssets(user.getSiteId(), user.getHospitalId(), groupBy, LocalDate.now().minusYears(1)), dmService.findAssets(user.getSiteId(), user.getHospitalId(), groupBy, LocalDate.now())))));
+    return serialize(request, groups, groupBy, averageUsage(usagePredict(dmService.findAssets(user.getSiteId(), user.getHospitalId(), groupBy, LocalDate.now().minusYears(1)),
+      dmService.findAssets(user.getSiteId(), user.getHospitalId(), groupBy, LocalDate.now()))));
   }
 
 
   /**
-   * <p>If usage is null the year before last year and not null last year, the forecast is the value of last year.
+   * <p>If the usage is null last year, this machine will not be included into the result
+   * If usage is null the year before last year and not null last year, the forecast is the value of last year.
    * If the usage is not null in both last year and the year before, the forecast is last year usage + standard deviation</p>
    *
    * @param theYearBeforeLast sql result the year before last year
@@ -57,59 +62,48 @@ public class DmApi {
    * @return forecast of next year
    */
   private Observable<Tuple5<Integer, Double, String, Integer, Double>> usagePredict(Observable<Tuple5<Integer, Double, String, Integer, Double>> theYearBeforeLast, Observable<Tuple5<Integer, Double, String, Integer, Double>> lastYear) {
+    log.info("got sql result: {}", theYearBeforeLast.count().toBlocking().single());
     return
       Observable.zip(theYearBeforeLast, lastYear, (lstSnd, lstFst) ->
         Tuple.of(lstSnd._1, lstSnd._2, lstSnd._3, lstSnd._4,
           lstSnd._5.equals(0D) ? lstFst._5 : lstFst._5 + Stats.of(lstSnd._5, lstFst._5).sampleStandardDeviation())).cache();
   }
 
-
-  private String calculateSuggestion(Double maxUsage, Double averageUsage) {
-    log.info("got averageUsage result, max: {}, mean: {}", maxUsage, averageUsage);
-    if (averageUsage > 1D) {
-      return "建议购买新设备";
-    } else if (maxUsage > 1D) {
-      return "建议合理安排使用";
-    } else if (averageUsage <= 0.3D) {
-      return "建议提高使用率";
-    } else {
-      return "";
-    }
-
-  }
-
-  private javaslang.collection.Map<Integer, Tuple3<Double, Double, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> averageUsage(Observable<Tuple5<Integer, Double, String, Integer, Double>> p) {
+  private Observable<Tuple3<Observable<Integer>, Observable<Double>, Observable<Tuple4<Integer, Double, String, Double>>>> averageUsage(Observable<Tuple5<Integer, Double, String, Integer, Double>> predictResult) {
     return
-      javaslang.collection.List.ofAll(p.toBlocking().toIterable()).groupBy(t -> t._4).mapValues(lst -> Tuple.of(
-        Stats.of(lst.map(t -> t._5).toJavaList()).mean(),
-        Stats.of(lst.map(t -> t._5).toJavaList()).max(),
-        lst.map(t -> Tuple.of(t._1, t._2, t._3, t._5))));
+      predictResult.cache().groupBy(k -> k._4).map(g -> Tuple.of(Observable.just(g.getKey()), MathObservable.averageDouble(g.map(v -> v._5)),
+        g.map(v -> Tuple.of(v._1, v._2, v._3, v._5)).cache())).cache();
   }
 
-  private Tuple2<Seq<ImmutableMap<String, Object>>, Double> mapItems(Map<Integer, String> groups, javaslang.collection.Map<Integer, Tuple3<Double, Double, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> items) {
-    return Tuple.of(
-      items.map((k, v) -> Tuple.of(k, new ImmutableMap.Builder<String, Object>()
-        .put("id", k)
-        .put("name", groups.get(k))
-        .put("usage", v._1)
-        .put("suggestion", calculateSuggestion(v._2, v._1))
-        .put("items", v._3.map(sub -> new ImmutableMap.Builder<String, Object>()
-          .put("id", sub._1)
-          .put("name", sub._3)
-          .put("size", sub._2)
-          .put("usage", sub._4)
-          .build()).toJavaList())
-        .build())).values(),
-      Stats.of(items.values().map(t -> t._1).toJavaList()).mean());
+
+  private String calculateSuggestion(Observable<Tuple3<Integer, Double, Observable<Tuple4<Integer, Double, String, Double>>>> usages) {
+    log.info("got averageUsage result: {}", usages.count().toBlocking().single());
+    return
+      usages.map(values -> {
+        if (values._2 > 1D) return "建议购买新设备";
+        else if (Stats.of(values._3.map(parameters -> parameters._4).toBlocking().toIterable()).max() > 1D)
+          return "建议合理安排使用";
+        else if (values._2 <= 0.3D) return "建议提高使用率";
+        else return "";
+      }).toBlocking().single();
   }
 
-  private ResponseEntity<Map<String, Object>> serialize(HttpServletRequest request, String groupBy, Tuple2<Seq<ImmutableMap<String, Object>>, Double> items) {
-    log.info("got final input items: {}", items._1.toJavaList());
+
+  private ResponseEntity<Map<String, Object>> serialize(HttpServletRequest request, Map<Integer, String> groups, String groupBy, Observable<Tuple3<Observable<Integer>, Observable<Double>, Observable<Tuple4<Integer, Double, String, Double>>>> items) {
     return ResponseEntity.ok().cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS)).body(new ImmutableMap.Builder<String, Object>()
       .put("id", "100")
       .put("name", "All Assets")
-      .put("usage", items._2)
-      .put("items", items._1.toJavaList())
+      //.put("usage", Stats.meanOf(items.flatMap(t -> t._2).toBlocking().toIterable()))
+      .put("items", items.flatMap(t -> Observable.zip(t._1, t._2, t._3, Tuple::of)).map(v -> new ImmutableMap.Builder<String, Object>()
+        .put("id", v._1)
+        .put("name", groups.get(v._1))
+        .put("usage", v._2)
+        .put("items", new ImmutableMap.Builder<String, Object>()
+          .put("id", v._3._1)
+          .put("name", v._3._3)
+          .put("size", v._3._2)
+          .put("usage", v._3._4))
+        .build()).cache().toBlocking().toIterable())
       .put("link", new ImmutableMap.Builder<String, Object>()
         .put("ref", "self")
         .put("href", String.format("%s?groupby=%s", request.getRequestURL(), groupBy))
@@ -117,4 +111,3 @@ public class DmApi {
       .build());
   }
 }
-
