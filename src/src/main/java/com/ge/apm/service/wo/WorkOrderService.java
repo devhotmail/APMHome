9374ead90696ed565dcc2a8f2087cefd5a48d46d
gaolei <43575218@qq.com>
2017-03-17 18:02:27 -0700

    work order update

--- src/src/main/java/com/ge/apm/service/wo/WorkOrderService.java
+++ src/src/main/java/com/ge/apm/service/wo/WorkOrderService.java
@@ -30,7 +30,7 @@ import java.util.List;
 @Service
 public class WorkOrderService {
     private static final Logger logger = Logger.getLogger(WorkOrderService.class);
-    
+
     @Autowired
     private SiteInfoRepository siteDao;
     @Autowired
@@ -67,23 +67,23 @@ public class WorkOrderService {
 
         woStep.setWorkOrderId(wo.getId());
         woStep.setSiteId(wo.getSiteId());
-        
+
         if(wo.getCurrentPersonId()!=null) woStep.setOwnerId(wo.getCurrentPersonId());
         if(wo.getCurrentPersonName()!=null) woStep.setOwnerName(wo.getCurrentPersonName());
-        
+
         woStep.setStepId(wo.getCurrentStepId());
         woStep.setStepName(WebUtil.getMessage("woSteps"+"-"+wo.getCurrentStepId()));
 
         woStep.setStartTime(TimeUtil.now());
-        
+
         return woStep;
     }
-    
+
     @Transactional
     public void saveWorkOrderStep(WorkOrder wo, WorkOrderStep currentWoStep, WorkOrderStep nextWoStep) throws RuntimeException{
         WorkOrderRepository woDao = WebUtil.getBean(WorkOrderRepository.class);
         WorkOrderStepRepository woStepDao = WebUtil.getBean(WorkOrderStepRepository.class);
-        
+
         if(wo!=null){
             try{
                 wo.setCurrentStepName(WebUtil.getMessage("woSteps"+"-"+wo.getCurrentStepId()));
@@ -93,12 +93,12 @@ public class WorkOrderService {
                 throw new RuntimeException("Failed to save WorkOrder:"+ex.getMessage());
             }
         }
-        
+
         if(currentWoStep!=null){
             if(wo!=null){
                 currentWoStep.setWorkOrderId(wo.getId());
             }
-            
+
             try{
                 currentWoStep.removeEmptyStepDetailRecord();
                 woStepDao.save(currentWoStep);
@@ -113,7 +113,7 @@ public class WorkOrderService {
             if(wo!=null){
                 nextWoStep.setWorkOrderId(wo.getId());
             }
-            
+
             try{
                 nextWoStep.removeEmptyStepDetailRecord();
                 woStepDao.save(nextWoStep);
@@ -127,20 +127,20 @@ public class WorkOrderService {
     protected void checkWorkOrderPersonNames(WorkOrder wo){
         UaaService uaaService = WebUtil.getBean(UaaService.class);
         UserAccount user;
-        
+
         user = uaaService.getUserById(wo.getRequestorId());
         if(user!=null)
             wo.setRequestorName(user.getName());
-        else 
+        else
             wo.setRequestorName("N/A");
-        
+
         user = uaaService.getUserById(wo.getCurrentPersonId());
         if(user!=null)
             wo.setCurrentPersonName(user.getName());
         else
             wo.setCurrentPersonName("N/A");
     }
-    
+
     public void createWorkOrderStep(WorkOrder wo, WorkOrderStep currentWoStep) throws Exception{
         finishWorkOrderStep(wo, currentWoStep);
     }
@@ -148,13 +148,13 @@ public class WorkOrderService {
     public void finishWorkOrderStep(WorkOrder wo, WorkOrderStep currentWoStep) throws Exception{
         // update current Work Order Step
         currentWoStep.setEndTime(TimeUtil.now());
-        
+
         // initiate next Work Order Step
         wo.setCurrentStepId(currentWoStep.getStepId()+1);
         checkWorkOrderPersonNames(wo);
-        
+
         WorkOrderStep nextWoStep = initWorkOrderCurrentStep(wo);
-        
+
         try{
             saveWorkOrderStep(wo, currentWoStep, nextWoStep);
         }
@@ -163,12 +163,12 @@ public class WorkOrderService {
             wo.setCurrentStepId(currentWoStep.getStepId());
             wo.setCurrentPersonId(currentWoStep.getOwnerId());
             wo.setCurrentPersonName(currentWoStep.getOwnerName());
-            
+
             throw ex;
         }
         //do wechat push
         sendTemplateMsg(wo, currentWoStep, nextWoStep);
-        
+
         //judge whether continue, if auto_stepX is true then continue
         int currentStep = wo.getCurrentStepId();
         if ( wo.getSiteId() == null) return;
@@ -193,10 +193,10 @@ public class WorkOrderService {
     public void closeWorkOrder(WorkOrder wo, WorkOrderStep currentWoStep) throws Exception{
         wo.setCurrentStepId(6);
         currentWoStep.setEndTime(TimeUtil.now());
-        
+
         WorkOrderRepository woDao = WebUtil.getBean(WorkOrderRepository.class);
         WorkOrderStepRepository woStepDao = WebUtil.getBean(WorkOrderStepRepository.class);
-        
+
         try{
             if (currentWoStep.getStepId() == 6){
                 saveWorkOrderStep(wo, currentWoStep, null);
@@ -208,24 +208,24 @@ public class WorkOrderService {
         }
         catch(Exception ex){
             throw ex;
-        }        
+        }
     }
 
     @Transactional
     public void transferWorkOrder(WorkOrder wo, WorkOrderStep currentWoStep) throws Exception{
         checkWorkOrderPersonNames(wo);
         WorkOrderStep nextWoStep = initWorkOrderCurrentStep(wo);
-        
+
         try{
             saveWorkOrderStep(wo, currentWoStep, nextWoStep);
         }
         catch(Exception ex){
             throw ex;
-        }        
+        }
         //do wechat push
         sendTemplateMsg(wo, currentWoStep, null);
     }
-    
+
     private void saveTotalTimeAndPrice(WorkOrderRepository woDao, WorkOrder wo, WorkOrderStep wos) {
         int totalManHour = wo.getTotalManHour();
         double totalPrice = wo.getTotalPrice();
@@ -237,15 +237,15 @@ public class WorkOrderService {
         if (details == null || details.isEmpty()) return;
         for (WorkOrderStepDetail detail : details) {
             totalManHour += detail.getManHours()==null?0:detail.getManHours();
-            totalPrice = totalPrice + (detail.getManHours()==null?0:detail.getManHours())*hourPrice 
-                    + (detail.getAccessoryQuantity()==null?0:detail.getAccessoryQuantity())*(detail.getAccessoryPrice()==null?0.0:detail.getAccessoryPrice()) 
+            totalPrice = totalPrice + (detail.getManHours()==null?0:detail.getManHours())*hourPrice
+                    + (detail.getAccessoryQuantity()==null?0:detail.getAccessoryQuantity())*(detail.getAccessoryPrice()==null?0.0:detail.getAccessoryPrice())
                     + (detail.getOtherExpense()==null?0.0:detail.getOtherExpense());
         }
         wo.setTotalManHour(totalManHour);
         wo.setTotalPrice(totalPrice);
         woDao.save(wo);
     }
-    
+
     //do wechat push
     @Autowired
     private  HttpServletRequest request;
@@ -257,22 +257,22 @@ public class WorkOrderService {
                     .toUser(user.getWeChatId()).templateId("4N0nfZ0fXstReD-FcBu-d6tUsTcwBEIND-0wmOh0cO8").build();
             if (currentWoStep.getStepId() == 6){
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("first", "工单已关单","#FF00FF"));
+                        new WxMpTemplateData("first", "工单已关单","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("performance", "工单已关单","#FF00FF"));
+                        new WxMpTemplateData("performance", "工单已关单","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("time", "2017-3-13 17:23:34","#FF00FF"));
+                        new WxMpTemplateData("time", "2017-3-13 17:23:34","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("remark", currentWoStep.getStepName()+"已完成，工单已关单","#FF00FF"));
+                        new WxMpTemplateData("remark", currentWoStep.getStepName()+"已完成，工单已关单","#FF00FF"));
             } else {
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("first", "工单已关单","#FF00FF"));
+                        new WxMpTemplateData("first", "工单已关单","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("performance", "工单已关单","#FF00FF"));
+                        new WxMpTemplateData("performance", "工单已关单","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("time", "2017-3-13 17:23:34","#FF00FF"));
+                        new WxMpTemplateData("time", "2017-3-13 17:23:34","#FF00FF"));
                 templateMessage.addWxMpTemplateData(
-                    new WxMpTemplateData("remark", currentWoStep.getStepName()+"已完成，当前状态为"+nextWoStep.getStepName(),"#FF00FF"));
+                        new WxMpTemplateData("remark", currentWoStep.getStepName()+"已完成，当前状态为"+nextWoStep.getStepName(),"#FF00FF"));
             }
             String serverName = request.getRequestURL().toString().replace("/web/finishwo", "");
             templateMessage.setUrl(wxService.oauth2buildAuthorizationUrl(serverName+"/web/menu/31", WxConsts.OAUTH2_SCOPE_USER_INFO, ""));
@@ -283,5 +283,5 @@ public class WorkOrderService {
             }
         }
     }
-    
+
 }
