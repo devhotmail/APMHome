9a0f96d801cc14dbdac8781810e59fbb36069c33
lihao <lihao.zhang1@ge.com>
2017-03-22 16:28:19 +0800

    adjustions for api: change the format and content of suggestion

--- src/src/main/java/com/ge/apm/web/DmApi.java
+++ src/src/main/java/com/ge/apm/web/DmApi.java
@@ -3,10 +3,14 @@ package com.ge.apm.web;
 import com.ge.apm.domain.UserAccount;
 import com.ge.apm.service.api.CommonService;
 import com.ge.apm.service.api.DmService;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.math.Stats;
 import com.google.common.primitives.Ints;
-import javaslang.*;
+import javaslang.Tuple;
+import javaslang.Tuple2;
+import javaslang.Tuple4;
+import javaslang.Tuple5;
 import javaslang.collection.Seq;
 import javaslang.control.Option;
 import org.slf4j.Logger;
@@ -22,6 +26,7 @@ import webapp.framework.web.service.UserContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -33,6 +38,9 @@ import static javaslang.API.*;
 @Validated
 public class DmApi {
   private static final Logger log = LoggerFactory.getLogger(DmApi.class);
+  private final String SUGGESTION_BUY = "建议购买";
+  private final String SUGGESTION_ADJUST = "建议合理安排使用";
+  private final String SUGGESTION_RAISE = "建议提高使用率";
 
   @Autowired
   private CommonService commonService;
@@ -67,31 +75,49 @@ public class DmApi {
   }
 
 
-  private String calculateSuggestion(Double maxUsage, Double averageUsage) {
-    return Match(Tuple.of(maxUsage, averageUsage)).of(
-      Case($(t -> t._2 > 1D), "建议购买新设备"),
-      Case($(t -> t._1 > 1D), "建议合理安排使用"),
-      Case($(t -> t._2 <= 0.3D), "建议提高使用率"),
-      Case($(), "")
-    );
+  private Map<String, String> calculateSuggestion(Integer NumGreaterThan1, Double averageUsage, Integer numOfAssets) {
+    return Match(Tuple.of(NumGreaterThan1, averageUsage)).of(
+      Case($(t -> t._2 > 1D), ImmutableMap.of("title", SUGGESTION_BUY, "addition", String.format("%s台设备", (int) Math.ceil((averageUsage - 1) * numOfAssets)))),
+      Case($(t -> t._1 > 0), ImmutableMap.of("title", SUGGESTION_ADJUST)),
+      Case($(t -> t._2 <= 0.3D), ImmutableMap.of("title", SUGGESTION_RAISE)),
+      Case($(), ImmutableMap.of("title", "")));
   }
 
-  private javaslang.collection.Map<Integer, Tuple3<Double, Double, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> averageUsage(Observable<Tuple5<Integer, Double, String, Integer, Double>> p) {
+  private List<Map<String, String>> calculateTotalSuggestion(Seq<ImmutableMap<String, Object>> items, String groupBy) {
+    Seq<String> suggestions = items.map(v -> ((ImmutableMap<String, String>) v.get("suggestions")).get("title"));
+    int numBuy = suggestions.count(v -> v.equals(SUGGESTION_BUY));
+    int numAjst = suggestions.count(v -> v.equals(SUGGESTION_ADJUST));
+    int numRse = suggestions.count(v -> v.equals(SUGGESTION_RAISE));
+    ImmutableList.Builder<Map<String, String>> totalSuggestions = new ImmutableList.Builder<Map<String, String>>();
+    if (numBuy > 0) {
+      totalSuggestions.add(ImmutableMap.of("title", SUGGESTION_BUY, "addition", Option.when(groupBy.equals("dept"), String.format("%s个科室", numBuy)).getOrElse(String.format("%s种类型", numBuy))));
+    }
+    if (numAjst > 0) {
+      totalSuggestions.add(ImmutableMap.of("title", SUGGESTION_ADJUST, "addition", Option.when(groupBy.equals("dept"), String.format("%s个科室", numAjst)).getOrElse(String.format("%s种类型", numAjst))));
+    }
+    if (numRse > 0) {
+      totalSuggestions.add(ImmutableMap.of("title", SUGGESTION_RAISE, "addition", Option.when(groupBy.equals("dept"), String.format("%s个科室", numRse)).getOrElse(String.format("%s种类型", numRse))));
+    }
+    return totalSuggestions.build();
+  }
+
+  private javaslang.collection.Map<Integer, Tuple4<Double, Integer, Integer, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> averageUsage(Observable<Tuple5<Integer, Double, String, Integer, Double>> p) {
     return
       javaslang.collection.List.ofAll(p.toBlocking().toIterable()).groupBy(t -> t._4).mapValues(lst -> Tuple.of(
         Stats.of(lst.map(t -> t._5).toJavaList()).mean(),
-        Stats.of(lst.map(t -> t._5).toJavaList()).max(),
+        lst.map(t -> t._5).count(v -> v > 1D),
+        lst.map(t -> t._5).count(v -> true),
         lst.map(t -> Tuple.of(t._1, t._2, t._3, t._5))));
   }
 
-  private Tuple2<Seq<ImmutableMap<String, Object>>, Double> mapItems(Map<Integer, String> groups, javaslang.collection.Map<Integer, Tuple3<Double, Double, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> items) {
+  private Tuple2<Seq<ImmutableMap<String, Object>>, Double> mapItems(Map<Integer, String> groups, javaslang.collection.Map<Integer, Tuple4<Double, Integer, Integer, javaslang.collection.List<Tuple4<Integer, Double, String, Double>>>> items) {
     return Tuple.of(
       items.map((k, v) -> Tuple.of(k, new ImmutableMap.Builder<String, Object>()
         .put("id", Option.of(k).getOrElseThrow(() -> new IllegalArgumentException(String.format("group Id should not be %s", k))))
         .put("name", Option.of(groups.get(k)).getOrElseThrow(() -> new IllegalArgumentException(String.format("group name should not be %s", groups.get(k)))))
         .put("usage", v._1)
-        .put("suggestion", calculateSuggestion(v._2, v._1))
-        .put("items", v._3.map(sub -> new ImmutableMap.Builder<String, Object>()
+        .put("suggestions", calculateSuggestion(v._2, v._1, v._3))
+        .put("items", v._4.map(sub -> new ImmutableMap.Builder<String, Object>()
           .put("id", sub._1)
           .put("name", Option.of(sub._3).getOrElseThrow(() -> new IllegalArgumentException(String.format("Asset name should not be %s", sub._3))))
           .put("size", Option.of(sub._2).getOrElseThrow(() -> new IllegalArgumentException(String.format("Purchase price should not be %s", sub._2))))
@@ -107,6 +133,7 @@ public class DmApi {
       .put("id", "100")
       .put("name", "All Assets")
       .put("usage", items._2)
+      .put("suggestions", calculateTotalSuggestion(items._1, groupBy))
       .put("items", items._1.toJavaList())
       .put("link", new ImmutableMap.Builder<String, Object>()
         .put("ref", "self")
