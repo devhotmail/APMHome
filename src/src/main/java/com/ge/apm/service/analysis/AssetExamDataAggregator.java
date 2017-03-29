1d0b8fbd46595f1f3c7caad2dd5b0d29d0225793
gaolei\lsg <43575218@qq.com>
2017-03-30 00:08:37 +0800

    test aggreate by day

--- src/src/main/java/com/ge/apm/service/analysis/AssetExamDataAggregator.java
+++ src/src/main/java/com/ge/apm/service/analysis/AssetExamDataAggregator.java
@@ -30,57 +30,9 @@ public class AssetExamDataAggregator {
     @Autowired
     AssetSummitRepository assetSummitRepository;
 
-    /*以examdate,siteid,hospitalid,assetid聚合clinical_record的priceAmount等数据，
-      并更新至asset_summit的revenue
-      PostgreSQL的update不支持双表语法, 使用两个hashmap来减少嵌套for循环的关联数据的查询
-      */
-  /*  public String operatorAggregator_deprecated(List<AssetClinicalRecordPojo> acrpList){
-        HashMap<String,AssetClinicalRecordPojo> hmRepo= new HashMap<String,AssetClinicalRecordPojo>();
-        HashMap<String,AssetSummit> hmSumit= new HashMap<String,AssetSummit>();
-        //初始化hmRepo key
-        List<AssetSummit> summitList=assetSummitRepository.getAssetSummit();
-        for(AssetClinicalRecordPojo accr:acrpList){
-            hmRepo.put(accr.getAssetIds()+"-"+accr.getHospitalIds()+"-"+accr.getSiteIds()+":"+accr.getExamDate(),accr);
-        }
-
-        //初始化hm2 key
-        for(AssetSummit asm :summitList){
-            hmSumit.put(asm.getAssetId()+"-"+asm.getHospitalId()+"-"+asm.getSiteId()+":"+asm.getCreated(),asm);
-        }
-
-        //hmRepo 是 clinical_record表 hmSumit是_summit表索引  两个key都为assetid-hospitalid-siteid
-        try {
-            List<AssetSummit> asmupdateList= new ArrayList<AssetSummit>();
-            Iterator iter =hmRepo.entrySet().iterator();
-            while(iter.hasNext()){
-                Map.Entry entry = (Map.Entry) iter.next();
-                Object key = entry.getKey();
-                if(hmSumit.containsKey(key)){
-                    AssetClinicalRecordPojo accrp = hmRepo.get(key);
-                    AssetSummit asm = hmSumit.get(key);
-                    asm.setRevenue((Math.round(accrp.getPriceAmounts()*100.0)/100.0));
-                    asm.setExposeCount(accrp.getExposeCounts());
-                    asm.setFilmCount(accrp.getFilmCounts());
-                    asm.setInjectCount(accrp.getInjectCounts());
-                    asm.setExamCount(accrp.getExamCount().intValue());
-                    asmupdateList.add(asm);
-                }else{ //在submit中不存在的 需要加入
-
-                }
-            }
-            assetSummitRepository.save(asmupdateList);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "failure";
-        }
-        return "success";
-    }
-*/
-
     public String aggregateExamData(){
         List<AssetClinicalRecordPojo> acrpList = assetClinicalRecordRepository.getAssetExamDataAggregator();
         logger.info("Asset Clinical Record size {}",acrpList.size());
-
         return "success";
     }
 
@@ -94,11 +46,8 @@ public class AssetExamDataAggregator {
             return "failure";
         }*/
 
-        AssetSummit assetSummit=null;
         List<AssetSummit> asmNewList= new ArrayList<AssetSummit>();
         List<AssetSummit> asmUpdateList= new ArrayList<AssetSummit>();
-
-        ////
         for(AssetClinicalRecordPojo accrp:acrpList){
             AssetSummit asm1 =assetSummitRepository.getAssetSummitByAssetIdAndCreated(accrp.getAssetIds(),date);
             if(asm1!=null){
@@ -118,7 +67,6 @@ public class AssetExamDataAggregator {
                 asm.setAssetId(accrp.getAssetIds());
                 asm.setHospitalId(accrp.getHospitalIds());
                 asm.setSiteId(accrp.getSiteIds());
-               // System.out.println("---insert----"+accrp.getAssetIds()+"---"+date);
                 asm.setExposeCount(accrp.getExposeCounts());
                 asm.setFilmCount(accrp.getFilmCounts());
                 asm.setInjectCount(accrp.getInjectCounts());
@@ -129,25 +77,17 @@ public class AssetExamDataAggregator {
             }
         }
         if(asmNewList.size()>0) {
-            System.out.println("new records are inserted into");
+            logger.info("new records are inserted into");
             assetSummitRepository.save(asmNewList);
         }
         if(asmUpdateList.size()>0){
-            System.out.println("there are records to be updated");
+            logger.info("there are records to be updated-");
             for(AssetSummit asm :asmUpdateList){
                 assetSummitRepository.updateAssetSummit(asm.getExposeCount(),asm.getInjectCount(),asm.getFilmCount(),asm.getId());
             }
         }
         return "success";
 
-    }
-
-
-    public void test(){
-        DateTime dtFrom = TimeUtil.timeNow().minusDays(100);
-        DateTime dtTo = dtFrom.plusDays(5);
-
-        initAssetAggregationDataByDateRange(dtFrom.toDate(), dtTo.toDate());
     }
 
     public void initAssetAggregationDataByDateRange(Date fromDate, Date toDate){
