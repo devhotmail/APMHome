63a93bed596086e844b8e9e587509b2f884026f1
Wu <212547631@logon.ds.ge.com>
2017-04-06 14:46:25 +0800

    update sql script

--- src/sql/init_data.sql
+++ src/sql/init_data.sql
@@ -8,50 +8,41 @@ truncate table data_table_config cascade;
 truncate table field_code_type cascade;
 
 
-INSERT INTO "site_info" VALUES ('1', 'GE', 'GE', 'GE', 'GE', null, null, null, null, null, null, null, true, false,false, false, false,false);
-INSERT INTO "site_info" VALUES ('2', 'Demo医联体', 'DemoHospitalGrup', '医联体', '医联体', null, null, null, null, null, null, null, true, false,false, false, false,false);
+INSERT INTO site_info(id,name,name_en,alias_name,site_description,contact_person,contact_phone,contact_email,location,location_en,time_zone,default_lang,is_enabled,wf_auto_step2,wf_auto_step3,wf_auto_step4,wf_auto_step5,wf_auto_step6) VALUES ('1', 'GE', 'GE', 'GE', 'GE', null, null, null, null, null, null, null, true, false,false, false, false,false);
+INSERT INTO site_info(id,name,name_en,alias_name,site_description,contact_person,contact_phone,contact_email,location,location_en,time_zone,default_lang,is_enabled,wf_auto_step2,wf_auto_step3,wf_auto_step4,wf_auto_step5,wf_auto_step6) VALUES ('2', 'Demo医联体', 'DemoHospitalGrup', '医联体', '医联体', null, null, null, null, null, null, null, true, false,false, false, false,false);
 
-INSERT INTO "org_info" VALUES ('1', '1', null, 'Digital Healthcare', 'Digital Healthcare', null);
-INSERT INTO "org_info" VALUES ('2', '2', null, '医院本部', 'Hospital Headquarter', null);
-INSERT INTO "org_info" VALUES ('3', '2', null, '浦东分院', 'Hospital Pudong branch', null);
-INSERT INTO "org_info" VALUES ('4', '2', 2, '放射科', null, '2');
-INSERT INTO "org_info" VALUES ('5', '2', 2, '超声室', 'cariology Dept', '2');
-INSERT INTO "org_info" VALUES ('6', '2', 2, '设备科', 'Asset Dept', '2');
-
-
-INSERT INTO "public"."sys_role" VALUES ('1', 'HospitalHead', '院长', '/homeHead.xhtml');
-INSERT INTO "public"."sys_role" VALUES ('2', 'AssetHead', '设备科主任', '/homeAssetHead.xhtml');
-INSERT INTO "public"."sys_role" VALUES ('3', 'AssetStaff', '设备科科员', '/homeAssetStaff.xhtml');
-INSERT INTO "public"."sys_role" VALUES ('4', 'DeptHead', '科室主任', '/homeDeptHead.xhtml');
-INSERT INTO "public"."sys_role" VALUES ('5', 'ITAdmin', 'IT管理员', '/portal/uaa/userAccount/List.xhtml');
-INSERT INTO "public"."sys_role" VALUES ('6', 'MultiHospital', '多院区管理', '');
-
-INSERT INTO user_account VALUES ('1', '1', '1', '1', 'admin', 'administrator', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'apm.admin@ge.com', '', 't', 't', 't', 't', 't', '2016-10-17 10:57:00');
-INSERT INTO user_account VALUES ('2', '2', '2', '2', 'head', '院长', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'head@a.com', null, 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('3', '2', '2', '6', 'assetHead', '设备科主任', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'assethead@a.com', '', 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('4', '2', '2', '4', 'deptHead', '放射科主任', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'radiologyHead@a.com', null, 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('5', '2', '2', '6', 'user', '科员', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'asset.staff@a.com', null, 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('6', '2', '2', '2', 'HPVP', 'Head of Hospital', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'head@a.com', null, 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('7', '2', '2', '6', 'EQPHead', 'Head of Equipment Dept', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'assethead@a.com', '', 'f', 'f', 'f', 't', 'f', null);
-INSERT INTO user_account VALUES ('8', '2', '2', '4', 'RADHead', 'Head of Radiology Dept', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'radiologyHead@a.com', null, 'f', 'f', 'f', 't', 'f', null);
-
-INSERT INTO "public"."user_role" VALUES ('1', '2', '1');
-INSERT INTO "public"."user_role" VALUES ('2', '3', '2');
-INSERT INTO "public"."user_role" VALUES ('3', '4', '4');
-INSERT INTO "public"."user_role" VALUES ('4', '5', '5');
-INSERT INTO "public"."user_role" VALUES ('5', '5', '3');
-INSERT INTO "public"."user_role" VALUES ('6', '6', '1');
-INSERT INTO "public"."user_role" VALUES ('7', '7', '2');
-INSERT INTO "public"."user_role" VALUES ('8', '8', '4');
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('1', '1', null, 'Digital Healthcare', 'Digital Healthcare', null);
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('2', '2', null, '医院本部', 'Hospital Headquarter', null);
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('3', '2', null, '浦东分院', 'Hospital Pudong branch', null);
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('4', '2', 2, '放射科', null, '2');
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('5', '2', 2, '超声室', 'cariology Dept', '2');
+INSERT INTO org_info(id,site_id,hospital_id,name,name_en,parent_id) values ('6', '2', 2, '设备科', 'Asset Dept', '2');
 
 
-INSERT INTO "chart_config" VALUES (1, 'dashboard_pie', 'select * from demo_data where id>=990 ', 't', 'pie', 'type', 'number1', 'number1', '饼图测试', 'e', NULL, 2, 't', 'x Label', NULL, NULL, NULL, NULL, NULL, NULL, 'y Label', NULL, NULL, 't');
-INSERT INTO "chart_config" VALUES (2, 'dashboard_bar', 'select * from demo_data where true :#searchFilter', 't', 'bar', 'type', 'the_date', 'number3', '柱状图测试', 'e', NULL, 2, 't', 'x Label', '', '', '', '', '', '', 'y Label', '', '', 't');
-INSERT INTO "chart_config" VALUES (3, 'dashboard_line', 'select * from demo_data where true :#searchFilter', 't', 'line', 'type', 'the_date', 'number4', '趋势图', 'e', NULL, 2, 't', 'X', '', '', '', '', '', '', 'YY', '', '', 'f');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('1', 'HospitalHead', '院长', '/homeHead.xhtml');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('2', 'AssetHead', '设备科主任', '/homeAssetHead.xhtml');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('3', 'AssetStaff', '设备科科员', '/homeAssetStaff.xhtml');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('4', 'DeptHead', '科室主任', '/homeDeptHead.xhtml');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('5', 'ITAdmin', 'IT管理员', '/portal/uaa/userAccount/List.xhtml');
+INSERT INTO sys_role(id,name,role_desc,home_page) values ('6', 'MultiHospital', '多院区管理', '');
 
-INSERT INTO "data_table_config" VALUES (1, 'test1', 'select count(*) from chart_config', 'select * from chart_config', 't', 't');
-INSERT INTO "data_table_config" VALUES (2, 'test2', 'select count(*) from demo_data where 1=1 :#searchFilter', 'select * from demo_data where 1=1 :#searchFilter', 't', 't');
-INSERT INTO "data_table_config" VALUES (3, 'aaa', 'aaa', 'aaa', 'f', 't');
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('1', '1', '1', '1', 'admin', 'administrator', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'apm.admin@ge.com', '', 't', 't', 't', 't', 't', '2016-10-17 10:57:00');
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('2', '2', '2', '2', 'head', '院长', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'head@a.com', null, 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('3', '2', '2', '6', 'assetHead', '设备科主任', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'assethead@a.com', '', 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('4', '2', '2', '4', 'deptHead', '放射科主任', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'radiologyHead@a.com', null, 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('5', '2', '2', '6', 'user', '科员', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'asset.staff@a.com', null, 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('6', '2', '2', '2', 'HPVP', 'Head of Hospital', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'head@a.com', null, 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('7', '2', '2', '6', 'EQPHead', 'Head of Equipment Dept', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'assethead@a.com', '', 'f', 'f', 'f', 't', 'f', null);
+INSERT INTO user_account(id,site_id,hospital_id,org_id,login_name,name,pwd_salt,password,email,telephone,is_super_admin,is_site_admin,is_local_admin,is_active,is_online,last_login_time) values ('8', '2', '2', '4', 'RADHead', 'Head of Radiology Dept', 'b380e7bb58d700d5','092561820bad0790cd3b3d5546809287c0cb17e67e6b8dd28665b292560298da', 'radiologyHead@a.com', null, 'f', 'f', 'f', 't', 'f', null);
+
+INSERT INTO user_role(id,user_id,role_id) VALUES ('1', '2', '1');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('2', '3', '2');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('3', '4', '4');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('4', '5', '5');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('5', '5', '3');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('6', '6', '1');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('7', '7', '2');
+INSERT INTO user_role(id,user_id,role_id) VALUES ('8', '8', '4');
 
 
 INSERT INTO field_code_type (id,msg_type,msg_type_name) VALUES (2,'caseSubType','故障子类别');
