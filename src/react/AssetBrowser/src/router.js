4fc784888cb0e1f2a650c235c32c7732eedd246e
Louis Yu <yuluyi1991@gmail.com>
2017-04-11 10:12:46 +0800

    feat: added asset browser

---
+++ src/react/AssetBrowser/src/router.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Route } from 'react-router'
+import AssetBrowser from '#/containers/AssetBrowser'
+
+// export default ({history}) => (
+//   <Router
+//     history={history}>
+//      <Route path="/" component={MonopolySim} />
+//   </Router>
+// )
+
+export default () => <AssetBrowser />
---
+++ src/react/AssetBrowser/tools/webpack.watch.conf.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const baseWebpackConfig = require('./webpack.base.conf')
+
+const utils = require('./utils')
+
+const publicPath = utils.getProdPublicPath()
+
+module.exports = merge(baseWebpackConfig, {
+  devtool: false,
+  output: {
+    filename: '[name].js',
+    publicPath,
+    sourceMapFilename: '[name].map'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.s[ca]ss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                module: true,
+                localIdentName: '[local]__[hash:base64:5]'
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|ico|svg)$/,
+        use: [
+          {
+            loader:'url-loader',
+            options: {
+              limit: 10000,
+              name: 'assets/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {'NODE_ENV': '"development"'}
+    }),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.xhtml',
+      filename: 'index.xhtml',
+      appMountId: 'root',
+      inject: false
+    }),
+    // Put all css code in this file
+    new ExtractTextPlugin('[name].css'),
+    new FriendlyErrorsPlugin()
+  ]
+})
