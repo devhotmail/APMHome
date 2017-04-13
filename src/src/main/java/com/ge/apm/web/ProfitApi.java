4a1d177b4f442e5bc3db2d081990d831e5d58f15
lihao <lihao.zhang1@ge.com>
2017-04-13 16:16:22 +0800

    new profit api(not logically right, just for front-end test purpose)

--- src/src/main/java/com/ge/apm/web/ProfitApi.java
+++ src/src/main/java/com/ge/apm/web/ProfitApi.java
@@ -14,7 +14,6 @@ import javaslang.collection.List;
 import javaslang.control.Option;
 import org.javamoney.moneta.Money;
 import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +22,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import rx.Observable;
-import rx.observables.MathObservable;
 import webapp.framework.web.service.UserContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -46,55 +45,61 @@ public class ProfitApi {
   @RequestMapping(method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<Map<String, Object>> calcProfit(HttpServletRequest request,
-                                                        @Min(2000) @RequestParam(name = "year", required = true) Integer year,
-                                                        @Pattern(regexp = "type|dept|month") @RequestParam(name = "groupby", required = false) String groupBy,
-                                                        @Min(1) @RequestParam(name = "type", required = false) Integer type,
-                                                        @Min(1) @RequestParam(name = "dept", required = false) Integer dept,
-                                                        @Min(1) @Max(12) @RequestParam(name = "month", required = false) Integer month,
-                                                        @Min(1) @Max(Integer.MAX_VALUE) @RequestParam(name = "limit", required = false) Integer limit,
-                                                        @Min(0) @RequestParam(name = "start", required = false, defaultValue = "0") Integer start) {
+                                                        @Min(2000) @RequestParam(value = "year", required = false) Integer year,
+                                                        @RequestParam(value = "from", required = false) String from,
+                                                        @RequestParam(value = "to", required = false) String to,
+                                                        @Pattern(regexp = "type|dept|month") @RequestParam(value = "groupby", required = false) String groupBy,
+                                                        @Min(1) @RequestParam(value = "type", required = false) Integer type,
+                                                        @Min(1) @RequestParam(value = "dept", required = false) Integer dept,
+                                                        @Min(1) @Max(12) @RequestParam(value = "month", required = false) Integer month,
+                                                        @Min(1) @Max(Integer.MAX_VALUE) @RequestParam(value = "limit", required = false) Integer limit,
+                                                        @Min(0) @RequestParam(value = "start", required = false, defaultValue = "0") Integer start) {
+    log.info("year:{}, groupby:{}, type:{}, dept:{}, month:{}, limit:{}, start:{}, from:{}, to:{}", year, groupBy, type, dept, month, limit, start, from, to);
+    LocalDate startDate;
+    LocalDate endDate;
     UserAccount user = UserContext.getCurrentLoginUser();
     Map<Integer, String> groups = Observable.from(commonService.findFields(user.getSiteId(), "assetGroup").entrySet()).filter(e -> Option.of(Ints.tryParse(e.getKey())).isDefined()).toMap(e -> Ints.tryParse(e.getKey()), Map.Entry::getValue).toBlocking().single();
     Map<Integer, String> depts = commonService.findDepts(user.getSiteId(), user.getHospitalId());
     Map<Integer, String> months = Observable.from(commonService.findFields(user.getSiteId(), "month").entrySet()).filter(e -> Option.of(Ints.tryParse(e.getKey())).isDefined()).toMap(e -> Ints.tryParse(e.getKey()), Map.Entry::getValue).toBlocking().single();
-    if (!Range.closed(DateTime.now().getYear() - 3, DateTime.now().getYear()).contains(year)) {
-      return ResponseEntity.badRequest().body(ImmutableMap.of("msg", "input data is not supported"));
-    } else if (Option.of(groupBy).isEmpty() && Option.of(type).isEmpty() && Option.of(dept).isEmpty() && Option.of(month).isEmpty()) {
-      return serialize(request, groups, depts, months,
-        calcChild(profitService.findRvnCstByYear(user.getSiteId(), user.getHospitalId(), year)),
-        year, groupBy, type, dept, month, limit, start);
-    } else if (Option.of(groupBy).isDefined() && Option.of(type).isEmpty() && Option.of(dept).isEmpty() && Option.of(month).isEmpty()) {
+    log.info("groups: {}, depts: {}, month: {}", groups, depts, months);
+    if (!(Option.of(from).isDefined() && Option.of(to).isDefined() && LocalDate.parse(from).compareTo(LocalDate.now().minusYears(3)) >= 0 && LocalDate.parse(to).compareTo(LocalDate.now()) <= 0)) {
+      if (Range.closed(DateTime.now().getYear() - 3, DateTime.now().getYear()).contains(year)) {
+        startDate = LocalDate.of(year, 1, 1);
+        endDate = LocalDate.of(year, 12, 31);
+      } else {
+        return ResponseEntity.badRequest().body(ImmutableMap.of("msg", "you must offer a valid time period"));
+      }
+    } else {
+      startDate = LocalDate.parse(from);
+      endDate = LocalDate.parse(to);
+    }
+    if (Option.of(groupBy).isDefined() && Option.of(type).isEmpty() && Option.of(month).isEmpty()) {
       return serialize(request, groups, depts, months,
         calcRoot(Option.when("type".equals(groupBy), groups).getOrElse(Option.when("dept".equals(groupBy), depts).getOrElse(months)),
-          profitService.findRvnCstGroupBy(user.getSiteId(), user.getHospitalId(), year, groupBy)),
-        year, groupBy, type, dept, month, limit, start);
-    } else if (Option.of(groupBy).isEmpty() && List.of(dept, type, month).count(v -> Option.of(v).isDefined()) == 1) {
+          profitService.findRvnCstForEachGroup(user.getSiteId(), user.getHospitalId(), startDate, endDate, groupBy, dept, month, type)),
+        startDate, endDate, groupBy, type, dept, month, limit, start);
+    } else if ((Option.of(groupBy).isEmpty() && List.of(type, month).count(v -> Option.of(v).isDefined()) == 1) ||
+      (Option.of(groupBy).isEmpty() && Option.of(month).isEmpty() && Option.of(type).isEmpty() && Option.of(dept).isDefined()) ||
+      (Option.of(groupBy).isEmpty() && Option.of(month).isEmpty() && Option.of(type).isEmpty() && Option.of(dept).isEmpty())) {
       return serialize(request, groups, depts, months,
-        calcChild(profitService.findRvnCstForEachType(user.getSiteId(), user.getHospitalId(), year,
-          Option.when(Option.of(type).isDefined(), "type").getOrElse(Option.when(Option.of(dept).isDefined(), "dept").getOrElse("month")),
-          Option.of(dept).orElse(Option.of(type)).getOrElse(month))),
-        year, groupBy, type, dept, month, limit, start);
+        calcChild(profitService.findRvnCstForEachItem(user.getSiteId(), user.getHospitalId(), startDate, endDate, dept, month, type)),
+        startDate, endDate, groupBy, type, dept, month, limit, start);
     } else {
       return ResponseEntity.badRequest().body(ImmutableMap.of("msg", "input data is not supported"));
     }
   }
 
-  private Observable<Tuple4<Integer, String, Money, Money>> calcRoot(Map<Integer, String> map, Observable<Tuple4<Integer, Money, Money, Money>> rvnCsts) {
-    return rvnCsts.map(v -> Tuple.of(v._1, map.get(v._1), v._2, v._2.subtract(v._3).subtract(v._4)))
+  private Observable<Tuple4<Integer, String, Money, Money>> calcRoot(Map<Integer, String> map, Observable<Tuple4<Integer, Money, Money, Money>> rvnCst) {
+    return rvnCst.map(v -> Tuple.of(v._1, map.get(v._1), v._2, v._2.subtract(v._3).subtract(v._4)))
       .filter(t -> Option.of(t._2).isDefined()).sorted((l, r) -> r._3.getNumber().intValue() - l._3.getNumber().intValue()).cache();
   }
 
-  private Observable<Tuple4<Integer, String, Money, Money>> calcChild(Observable<Tuple5<Integer, String, Money, Money, Money>> rvnCsts) {
-    return rvnCsts.map(v -> Tuple.of(v._1, v._2, v._3, v._3.subtract(v._4).subtract(v._5)))
+  private Observable<Tuple4<Integer, String, Money, Money>> calcChild(Observable<Tuple5<Integer, String, Money, Money, Money>> rvnCst) {
+    return rvnCst.map(v -> Tuple.of(v._1, v._2, v._3, v._3.subtract(v._4).subtract(v._5)))
       .filter(t -> Option.of(t._2).isDefined()).sorted((l, r) -> r._3.getNumber().intValue() - l._3.getNumber().intValue()).cache();
   }
 
-  private Observable<Tuple4<Integer, String, Money, Money>> forecastRvnCst(Observable<Tuple4<Integer, String, Money, Money>> rvnCsts) {
-    return Observable.zip(rvnCsts, Observable.zip(MathObservable.sumDouble(rvnCsts.map(t -> t._3.getNumber().doubleValue())).cache(), MathObservable.sumDouble(rvnCsts.map(t -> t._4.getNumber().doubleValue())).cache(), (r, p) -> Tuple.of(ProfitService.predictRevenue().getNumber().doubleValue(), r, p)).repeat(),
-      (l, r) -> Tuple.of(l._1, l._2, CNY.money(r._1 * l._3.getNumber().doubleValue() / r._2), CNY.money((r._1 * r._3 / r._2) * l._4.getNumber().doubleValue() / r._3)));
-  }
-
-  private Iterable<ImmutableMap<String, Object>> mapItems(HttpServletRequest request, Observable<Tuple4<Integer, String, Money, Money>> children, int year, String groupBy, Integer type, Integer dept, Integer month, Integer limit, Integer start) {
+  private Iterable<ImmutableMap<String, Object>> mapItems(HttpServletRequest request, Observable<Tuple4<Integer, String, Money, Money>> children, LocalDate from, LocalDate to, String groupBy, Integer type, Integer dept, Integer month, Integer limit, Integer start) {
     return children.skip(start).limit(Option.of(limit).getOrElse(Integer.MAX_VALUE)).map(child -> new ImmutableMap.Builder<String, Object>()
       .put("id", child._1)
       .put("name", Option.of(child._2).getOrElseThrow(() -> new IllegalArgumentException(String.format("snd value of %s is null", child))))
@@ -107,12 +112,12 @@ public class ProfitApi {
       .put("profit_label_unit", CNY.desc(child._4)._2)
       .put("link", new ImmutableMap.Builder<String, Object>()
         .put("ref", "child")
-        .put("href", Option.of(groupBy).map(v -> String.format("%s?year=%s&%s=%s", request.getRequestURL().toString(), year, groupBy, child._1)).getOrElse(""))
+        .put("href", Option.of(groupBy).map(v -> String.format("%s?from=%s&to=%s&%s=%s", request.getRequestURL().toString(), from, to, groupBy, child._1)).getOrElse(""))
         .build())
       .build()).cache().toBlocking().toIterable();
   }
 
-  private ResponseEntity<Map<String, Object>> serialize(HttpServletRequest request, Map<Integer, String> groups, Map<Integer, String> depts, Map<Integer, String> months, Observable<Tuple4<Integer, String, Money, Money>> children, int year, String groupBy, Integer type, Integer dept, Integer month, Integer limit, Integer start) {
+  private ResponseEntity<Map<String, Object>> serialize(HttpServletRequest request, Map<Integer, String> groups, Map<Integer, String> depts, Map<Integer, String> months, Observable<Tuple4<Integer, String, Money, Money>> children, LocalDate from, LocalDate to, String groupBy, Integer type, Integer dept, Integer month, Integer limit, Integer start) {
     Map<String, Object> body = new ImmutableMap.Builder<String, Object>()
       .put("pages", new ImmutableMap.Builder<String, Object>()
         .put("total", children.count().toBlocking().single())
@@ -136,60 +141,15 @@ public class ProfitApi {
         .put("profit_text", "总利润")
         .put("link", new ImmutableMap.Builder<String, Object>()
           .put("ref", "self")
-          .put("href", Option.of(groupBy).map(v -> String.format("%s?year=%s&groupby=%s", request.getRequestURL().toString(), year, v))
-            .orElse(Option.of(type).map(v -> String.format("%s?year=%s&type=%s", request.getRequestURL().toString(), year, v)))
-            .orElse(Option.of(dept).map(v -> String.format("%s?year=%s&dept=%s", request.getRequestURL().toString(), year, v)))
-            .orElse(Option.of(dept).map(v -> String.format("%s?year=%s&month=%s", request.getRequestURL().toString(), year, v)))
-            .getOrElse(String.format("%s?year=%s", request.getRequestURL(), year)))
+          .put("href", Option.of(groupBy).map(v -> String.format("%s?from=%s&to=%s&groupby=%s", request.getRequestURL().toString(), from, to, v))
+            .orElse(Option.of(type).map(v -> String.format("%s?from=%s&to=%s&groupby=%s", request.getRequestURL().toString(), from, to, v)))
+            .orElse(Option.of(dept).map(v -> String.format("%s?from=%s&to=%s&groupby=%s", request.getRequestURL().toString(), from, to, v)))
+            .orElse(Option.of(dept).map(v -> String.format("%s?from=%s&to=%s&groupby=%s", request.getRequestURL().toString(), from, to, v)))
+            .getOrElse(String.format("%s?from=%s&to=%s", request.getRequestURL(), from, to)))
           .build())
         .build())
-      .put("items", mapItems(request, children, year, groupBy, type, dept, month, limit, start))
+      .put("items", mapItems(request, children, from, to, groupBy, type, dept, month, limit, start))
       .build();
     return ResponseEntity.ok().cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS)).body(body);
   }
-
-  @RequestMapping(method = RequestMethod.GET, path = "/forecast")
-  @ResponseBody
-  public ResponseEntity<? extends Map<String, Object>> forecastProfit(HttpServletRequest request,
-//                                                                    @Future @RequestParam(name = "year", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") DateTime year,
-//                                                                    @Future @RequestParam(name = "from", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") DateTime from,
-//                                                                    @Future @RequestParam(name = "to", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") DateTime to,
-                                                                      @Pattern(regexp = "type|dept|month") @RequestParam(name = "groupby", required = false) String groupBy,
-                                                                      @Min(1) @RequestParam(name = "type", required = false) Integer type,
-                                                                      @Min(1) @RequestParam(name = "dept", required = false) Integer dept,
-                                                                      @Min(1) @Max(12) @RequestParam(name = "month", required = false) Integer month,
-                                                                      @Min(1) @Max(Integer.MAX_VALUE) @RequestParam(name = "limit", required = false) Integer limit,
-                                                                      @Min(0) @RequestParam(name = "start", required = false, defaultValue = "0") Integer start) {
-    UserAccount user = UserContext.getCurrentLoginUser();
-    Map<Integer, String> groups = Observable.from(commonService.findFields(user.getSiteId(), "assetGroup").entrySet()).filter(e -> Option.of(Ints.tryParse(e.getKey())).isDefined()).toMap(e -> Ints.tryParse(e.getKey()), Map.Entry::getValue).toBlocking().single();
-    Map<Integer, String> depts = commonService.findDepts(user.getSiteId(), user.getHospitalId());
-    Map<Integer, String> months = Observable.from(commonService.findFields(user.getSiteId(), "month").entrySet()).filter(e -> Option.of(Ints.tryParse(e.getKey())).isDefined()).toMap(e -> Ints.tryParse(e.getKey()), Map.Entry::getValue).toBlocking().single();
-//    return Match(Tuple.of(request, groupBy, type, dept, month, limit, start)).of(
-////      Case($(t -> List.of(t._2, t._3, t._4).forAll(p -> Option.of(p).isEmpty())), t -> serialize(request, groups, depts, months,
-////        calcChild(profitService.findRvnCstByYear(user.getSiteId(), user.getHospitalId(), year)),
-////        year, groupBy, type, dept, month, limit, start)),
-//      Case($(), ResponseEntity.badRequest().body(ImmutableMap.of("msg", "input data is not supported")))
-//    );
-    final int year = LocalDate.now().minusYears(1).getYear();
-    if (Option.of(groupBy).isEmpty() && Option.of(type).isEmpty() && Option.of(dept).isEmpty() && Option.of(month).isEmpty()) {
-      return serialize(request, groups, depts, months,
-        forecastRvnCst(calcChild(profitService.findRvnCstByYear(user.getSiteId(), user.getHospitalId(), year))),
-        year, groupBy, type, dept, month, limit, start);
-    } else if (Option.of(groupBy).isDefined() && Option.of(type).isEmpty() && Option.of(dept).isEmpty() && Option.of(month).isEmpty()) {
-      return serialize(request, groups, depts, months,
-        forecastRvnCst(calcRoot(Option.when("type".equals(groupBy), groups).getOrElse(Option.when("dept".equals(groupBy), depts).getOrElse(months)),
-          profitService.findRvnCstGroupBy(user.getSiteId(), user.getHospitalId(), year, groupBy))),
-        year, groupBy, type, dept, month, limit, start);
-    } else if (Option.of(groupBy).isEmpty() && List.of(dept, type, month).count(v -> Option.of(v).isDefined()) == 1) {
-      return serialize(request, groups, depts, months,
-        forecastRvnCst(calcChild(profitService.findRvnCstForEachType(user.getSiteId(), user.getHospitalId(), year,
-          Option.when(Option.of(type).isDefined(), "type").getOrElse(Option.when(Option.of(dept).isDefined(), "dept").getOrElse("month")),
-          Option.of(dept).orElse(Option.of(type)).getOrElse(month)))),
-        year, groupBy, type, dept, month, limit, start);
-    } else {
-      return ResponseEntity.badRequest().body(ImmutableMap.of("msg", "input data is not supported"));
-    }
-  }
-
-
 }
