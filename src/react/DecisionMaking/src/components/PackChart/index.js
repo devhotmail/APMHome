05737078789ca650f2e6db3684505109f621430e
Taian Lu <taian.lu@ge.com>
2017-05-09 09:29:42 +0800

    hotfix: module not found in dm page

--- src/react/DecisionMaking/src/components/PackChart/index.js
+++ src/react/DecisionMaking/src/components/PackChart/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 import React from 'react'
 
-import DataProvider from './DataProvider'
+import DataProvider from './dataProvider'
 import Chart from './chart'
 
 export default DataProvider(Chart)
