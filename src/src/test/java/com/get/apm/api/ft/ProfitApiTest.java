91dfbaa814e7116c7700b4da0523c05a0d647b16
lihao <lihao.zhang1@ge.com>
2017-05-10 16:16:11 +0800

    profit api forecast test and bug fix

--- src/src/test/java/com/get/apm/api/ft/ProfitApiTest.java
+++ src/src/test/java/com/get/apm/api/ft/ProfitApiTest.java
@@ -1,8 +1,12 @@
 package com.get.apm.api.ft;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.primitives.Doubles;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import okhttp3.ResponseBody;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Percentage;
 import org.junit.Before;
 import org.junit.Test;
 import retrofit2.Call;
@@ -10,9 +14,11 @@ import retrofit2.Response;
 import retrofit2.http.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Map;
 
 public class ProfitApiTest extends AbstractApiTest {
+  private final double accuracy = 0.01D;
   private ProfitApiTestsInterface tests;
 
   public ProfitApiTest() {
@@ -38,6 +44,36 @@ public class ProfitApiTest extends AbstractApiTest {
     Response response;
     response = call.execute();
     Assertions.assertThat(response.code()).isEqualTo(404);
+  }
+
+  private void rootEqualsSumOfChildrenTest(ProfitApiTestsInterface tests, Map<String, String> queryMap) throws IOException {
+    Response<ResponseBody> response = tests.profit(this.getCookie(), queryMap).execute();
+    Assertions.assertThat(response).isNotNull();
+    Assertions.assertThat(response.body()).isNotNull();
+    Assertions.assertThat(response.body().contentType().toString()).isEqualTo("application/json;charset=UTF-8");
+    Config parsedResponse = ConfigFactory.parseString(response.body().string());
+    Assertions.assertThat(Doubles.tryParse(parsedResponse.getString("root.revenue"))).isEqualTo(javaslang.collection.List.ofAll(parsedResponse.getConfigList("items")).map(v -> v.getString("revenue")).map(Doubles::tryParse).sum().doubleValue());
+    Assertions.assertThat(Doubles.tryParse(parsedResponse.getString("root.cost"))).isEqualTo(javaslang.collection.List.ofAll(parsedResponse.getConfigList("items")).map(v -> v.getString("cost")).map(Doubles::tryParse).sum().doubleValue());
+  }
+
+  private void consistencyForDifferentQueriesTest(ProfitApiTestsInterface tests, Map<String, String> queryMap1, Map<String, String> queryMap2) throws IOException {
+    Assertions.assertThat(Doubles.tryParse(ConfigFactory.parseString(tests.profit(this.getCookie(), queryMap1).execute().body().string()).getString("root.revenue"))).isEqualTo(Doubles.tryParse(ConfigFactory.parseString(tests.profit(this.getCookie(), queryMap2).execute().body().string()).getString("root.revenue")));
+    Assertions.assertThat(Doubles.tryParse(ConfigFactory.parseString(tests.profit(this.getCookie(), queryMap1).execute().body().string()).getString("root.cost"))).isEqualTo(Doubles.tryParse(ConfigFactory.parseString(tests.profit(this.getCookie(), queryMap2).execute().body().string()).getString("root.cost")));
+  }
+
+  private void rootEqualsSumOfChildrenForecastTest(ProfitApiTestsInterface tests, Map<String, String> queryMap) throws IOException {
+    Response<ResponseBody> response = tests.forecast(this.getCookie(), queryMap).execute();
+    Assertions.assertThat(response).isNotNull();
+    Assertions.assertThat(response.body()).isNotNull();
+    Assertions.assertThat(response.body().contentType().toString()).isEqualTo("application/json;charset=UTF-8");
+    Config parsedResponse = ConfigFactory.parseString(response.body().string());
+    Assertions.assertThat(Doubles.tryParse(parsedResponse.getString("root.revenue"))).isCloseTo(javaslang.collection.List.ofAll(parsedResponse.getConfigList("items")).map(v -> v.getString("revenue")).map(Doubles::tryParse).sum().doubleValue(), Percentage.withPercentage(accuracy));
+    Assertions.assertThat(Doubles.tryParse(parsedResponse.getString("root.cost"))).isCloseTo(javaslang.collection.List.ofAll(parsedResponse.getConfigList("items")).map(v -> v.getString("cost")).map(Doubles::tryParse).sum().doubleValue(), Percentage.withPercentage(accuracy));
+  }
+
+  private void consistencyForDifferentQueriesForecastTest(ProfitApiTestsInterface tests, Map<String, String> queryMap1, Map<String, String> queryMap2) throws IOException {
+    Assertions.assertThat(Doubles.tryParse(ConfigFactory.parseString(tests.forecast(this.getCookie(), queryMap1).execute().body().string()).getString("root.revenue"))).isCloseTo(Doubles.tryParse(ConfigFactory.parseString(tests.forecast(this.getCookie(), queryMap2).execute().body().string()).getString("root.revenue")), Percentage.withPercentage(accuracy));
+    Assertions.assertThat(Doubles.tryParse(ConfigFactory.parseString(tests.forecast(this.getCookie(), queryMap1).execute().body().string()).getString("root.cost"))).isCloseTo(Doubles.tryParse(ConfigFactory.parseString(tests.forecast(this.getCookie(), queryMap2).execute().body().string()).getString("root.cost")), Percentage.withPercentage(accuracy));
   }
 
   @Before
@@ -138,6 +174,12 @@ public class ProfitApiTest extends AbstractApiTest {
     doOkTest(tests, ImmutableMap.of("year", "2016", "month", "7", "start", "0"));
 
     doOkTest(tests, ImmutableMap.of("year", "2016", "month", "12", "limit", "10", "start", "6"));
+  }
+
+  @Test
+  public void testFromTo() throws IOException {
+    doOkTest(tests, ImmutableMap.of("from", "2015-12-12", "to", "2016-02-13"));
+    doOkTest(tests, ImmutableMap.of("from", "2015-12-22", "to", "2016-12-13"));
   }
 
 
@@ -247,6 +289,114 @@ public class ProfitApiTest extends AbstractApiTest {
     doNegativeTest(tests, ImmutableMap.of("year", "2016", "type", "4", "dept", "5", "month", "4"));
   }
 
+  @Test
+  public void testLackFromOrTo() throws IOException {
+    doNegativeTest(tests, ImmutableMap.of("from", LocalDate.now().minusYears(2).toString()));
+    doNegativeTest(tests, ImmutableMap.of("to", LocalDate.now().toString()));
+    doNegativeTest(tests, ImmutableMap.of("from", LocalDate.now().minusYears(2).toString(), "dept", "5"));
+  }
+
+  @Test
+  public void testWrongFromTo() throws IOException {
+    doNegativeTest(tests, ImmutableMap.of("from", LocalDate.now().minusYears(3).minusDays(1).toString(), "to", LocalDate.now().toString()));
+    doNegativeTest(tests, ImmutableMap.of("from", LocalDate.now().minusYears(3).minusDays(1).toString(), "to", LocalDate.now().plusDays(1).toString(), "dept", "5"));
+  }
+
+  //logical tests
+  @Test
+  public void testHeadGroupbyType() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "groupby", "type"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-05-06", "to", "2017-01-09", "groupby", "type"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "type", "2"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-04-10", "to", "2017-02-19", "type", "5"));
+  }
+
+  @Test
+  public void testHeadGroupbyDept() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2015", "groupby", "dept"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-06", "to", "2017-02-09", "groupby", "dept"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2015", "dept", "1"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-02", "to", "2016-12-19", "dept", "3"));
+  }
+
+  @Test
+  public void testHeadGroupbyMonth() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "groupby", "month"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-06", "to", "2016-11-09", "groupby", "month"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "month", "12"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-12", "to", "2016-11-19", "dept", "7"));
+  }
+
+  @Test
+  public void testHeadSingleMachine() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2015"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-07-06", "to", "2017-02-22"));
+  }
+
+  @Test
+  public void testDeptheadGroupbyType() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "groupby", "type", "dept", "1"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-05-06", "to", "2017-01-09", "groupby", "type", "dept", "2"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "type", "2", "dept", "3"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-04-10", "to", "2017-02-19", "type", "5", "dept", "4"));
+  }
+
+  @Test
+  public void testDeptheadGroupbyMonth() throws IOException {
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "groupby", "month", "dept", "1"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-06", "to", "2016-11-09", "groupby", "month", "dept", "2"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("year", "2016", "month", "12", "dept", "3"));
+    rootEqualsSumOfChildrenTest(tests, ImmutableMap.of("from", "2016-03-12", "to", "2016-11-19", "dept", "7"));
+  }
+
+  @Test
+  public void testConsistency() throws IOException {
+    consistencyForDifferentQueriesTest(tests, ImmutableMap.of("year", "2016", "groupby", "type"), ImmutableMap.of("from", "2016-01-01", "to", "2016-12-31", "groupby", "month"));
+    consistencyForDifferentQueriesTest(tests, ImmutableMap.of("year", "2016", "month", "12"), ImmutableMap.of("from", "2016-12-01", "to", "2016-12-31", "groupby", "type"));
+  }
+
+  //logical forecast tests
+  @Test
+  public void testHeadGroupbyTypeForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear(), "groupby", "type"));
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "type", "2"));
+  }
+
+  @Test
+  public void testHeadGroupbyDeptForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear(), "groupby", "dept"));
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "dept", "1"));
+  }
+
+  @Test
+  public void testHeadGroupbyMonthForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear(), "groupby", "month"));
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "month", "12"));
+  }
+
+  @Test
+  public void testHeadSingleMachineForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear()));
+  }
+
+  @Test
+  public void testDeptheadGroupbyTypeForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "groupby", "type", "dept", "1"));
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear(), "type", "2", "dept", "3"));
+  }
+
+  @Test
+  public void testDeptheadGroupbyMonthForecast() throws IOException {
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "groupby", "month", "dept", "1"));
+    rootEqualsSumOfChildrenForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear(), "month", "12", "dept", "3"));
+  }
+
+  @Test
+  public void testConsistencyForecast() throws IOException {
+    consistencyForDifferentQueriesForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().plusYears(1).getYear(), "groupby", "type"), ImmutableMap.of("from", LocalDate.now().withDayOfYear(1).plusYears(1).toString(), "to", LocalDate.now().withDayOfYear(1).plusYears(2).minusDays(1).toString(), "groupby", "month"));
+    consistencyForDifferentQueriesForecastTest(tests, ImmutableMap.of("year", "" + LocalDate.now().getYear()), ImmutableMap.of("from", LocalDate.now().withDayOfYear(1).plusMonths(11).toString(), "to", LocalDate.now().withDayOfYear(1).plusYears(1).minusDays(1).toString(), "groupby", "type"));
+  }
+
 }
 
 
@@ -254,6 +404,22 @@ interface ProfitApiTestsInterface {
   @Headers("Accept: application/json")
   @GET("api/profit")
   Call<ResponseBody> profit(@Header("Cookie") String cookie, @QueryMap Map<String, String> options);
+
+  @Headers("Accept: application/json")
+  @GET("api/profit/forecast")
+  Call<ResponseBody> forecast(@Header("Cookie") String cookie, @QueryMap Map<String, String> options);
+
+  @Headers("Accept: application/json")
+  @GET("api/profit/forecastrate")
+  Call<ResponseBody> forecastrate(@Header("Cookie") String cookie, @QueryMap Map<String, String> options);
+
+  @Headers("Accept: application/json")
+  @GET("api/profit/forecast")
+  Call<ResponseBody> forecastPut(@Header("Cookie") String cookie, @QueryMap Map<String, String> options, Object body);
+
+  @Headers("Accept: application/json")
+  @GET("api/profit/forecastrate")
+  Call<ResponseBody> forecastratePut(@Header("Cookie") String cookie, @QueryMap Map<String, String> options, Object body);
 
   @Headers("Accept: application/json")
   @GET("api/profit/{wrongPath}")
