b18134dc1f97e5511a60e8e53426de979af72475
Taian Lu <taian.lu@ge.com>
2017-05-12 13:28:11 +0800

    feat: dm use local fonts

--- src/react/DecisionMaking/tools/theme.js
+++ src/react/DecisionMaking/tools/theme.js
@@ -1,6 +1,22 @@
-module.exports = {
+const path = require('path')
+const config = require('../config')
+
+const theme = {
   '@btn-border-radius-base': '14px',
   '@border-color-base': '#bababa',
   '@border-color-split': '#bababa',
   '@btn-primary-bg': '#5b84d7'
 }
+
+const { NODE_ENV } = process.env
+if (~['watch', 'production'].indexOf(NODE_ENV)) {
+  const { commonPrefix = '/' } = config[process.env.NODE_ENV]
+
+  const iconUrl = path.join(commonPrefix, 'resources/fonts/anticon/iconfont')
+
+  Object.assign(theme, {
+    '@icon-url': JSON.stringify(iconUrl)
+  })
+}
+
+module.exports = theme
