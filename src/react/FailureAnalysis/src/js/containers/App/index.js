501da8ee7e7d8871131985554d0e00b8796ccc81
xiuzhu <nooooru@gmail.com>
2017-05-13 13:23:33 +0800

    Reverse not draft code

--- src/react/FailureAnalysis/src/js/containers/App/index.js
+++ src/react/FailureAnalysis/src/js/containers/App/index.js
@@ -42,17 +42,8 @@ const DisplayOptions = [
   { key: 'display_brand' },
   { key: 'display_dept' },
 ]
-function DataOrPlaceHolder(items, lastYear, placeholderSize) {
+function DataOrPlaceHolder(items, placeholderSize) {
   if (items && items.length && items[0].strips.type !== 'placeholder') {
-    if (lastYear && lastYear.length) {
-      items = items.map((item, i) => {
-        let lastYearItem = lastYear[i]
-        if (lastYearItem) {
-          lastYearItem.strips[0].color = colors.gray
-        }
-        lastYearItem && item.strips.concat(lastYear.strips)
-      })
-    }
     return items
   }
   return App.getPlaceholder(placeholderSize)
@@ -92,8 +83,8 @@ function mapDispatch2Porps(dispatch) {
 }
 
 function mapState2Props(state) {
-  let { parameters : { pagination, display, dataType, showLastYear } } = state
-  return { pagination, display, dataType, showLastYear }
+  let { parameters : { pagination, display, dataType } } = state
+  return { pagination, display, dataType }
 }
 
 @connect(mapState2Props, mapDispatch2Porps)
