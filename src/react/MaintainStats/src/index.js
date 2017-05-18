d4ed18ef81f15c1eacdbea31a47574066f23a76c
Taian Lu <taian.lu@ge.com>
2017-05-18 18:30:55 +0800

    feat: basic data and state control

--- src/react/MaintainStats/src/index.js
+++ src/react/MaintainStats/src/index.js
@@ -1,6 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import dva from 'dva'
+import { useRouterHistory } from 'dva/router'
+import createHashHistory from 'history/lib/createHashHistory'
 
-import App from './main'
+import './polyfill'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import router from './router'
+import models from './models'
+
+import './styles/index.scss'
+
+const historyEngine = useRouterHistory(createHashHistory)({
+  queryKey: false,
+  basename: '/'
+})
+
+const app = dva({
+  history: historyEngine,
+  onError: e => {
+    if (process.env.NODE_ENV === 'production') return
+    console.log(e)
+  }
+})
+
+models.forEach(model => app.model(model))
+
+app.router(router)
+
+app.start('#root')
