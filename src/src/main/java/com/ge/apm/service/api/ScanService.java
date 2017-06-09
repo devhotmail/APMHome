5e3f0b0deca4650a574d35b792e189dcb1d3a3f9
Ivar <ivar.chen@ge.com>
2017-06-09 19:40:41 +0800

    fixed sql query for scanApi

--- src/src/main/java/com/ge/apm/service/api/ScanService.java
+++ src/src/main/java/com/ge/apm/service/api/ScanService.java
@@ -33,14 +33,19 @@ public class ScanService {
     db = Database.from(connectionProvider);
   }
 
-
   @Cacheable(cacheNames = "springCache", key = "'ScanService.brief.'+#site+'.'+#hospital+'.'+#from+'.'+#to+'.'+#type+'.'+#dept+'.'+#asset")
   public Observable<Tuple3<Integer, Integer, Integer>> brief(int site, int hospital, Date from, Date to, Integer type, Integer dept, Integer asset) {
     Objects.requireNonNull(from);
     Objects.requireNonNull(to);
     QuerySelect.Builder builder = db.select(new SQL() {{
       SELECT("l.asset_group", "l.part_id", "COALESCE(r.exam_num,0)");
-      FROM("(select distinct asset_group, part_id from proc_map) as l");
+      FROM("(".concat(new SQL() {{
+        SELECT_DISTINCT("asset_group", "part_id");
+        FROM("proc_map");
+        if (Option.of(type).filter(i -> i > 0).isDefined()) {
+          WHERE("asset_group = :type");
+        }
+      }}.toString()).concat(") as l"));
       LEFT_OUTER_JOIN("(".concat(new SQL() {{
         SELECT("asset_group", "part_id", "sum(exam_count) as exam_num");
         FROM("exam_summit");
@@ -69,11 +74,30 @@ public class ScanService {
       .cache();
   }
 
+  //SELECT ai.id as asset_id, pm.asset_group, pm.part_id from asset_info ai join (select distinct asset_group, part_id from proc_map) as pm on ai.asset_group = pm.asset_group
   @Cacheable(cacheNames = "springCache", key = "'ScanService.assetDetail.'+#site+'.'+#hospital+'.'+#from+'.'+#to+'.'+#type+'.'+#dept+'.'+#asset+'.'+#part")
   public Observable<Tuple4<Integer, Integer, Integer, Integer>> assetDetail(int site, int hospital, Date from, Date to, Integer type, Integer dept, Integer asset, Integer part) {
     QuerySelect.Builder builder = db.select(new SQL() {{
-      SELECT("l.asset_group", "r.asset_id", "l.part_id", "COALESCE(r.exam_num,0)");
-      FROM("(select distinct asset_group, part_id from proc_map) as l");
+      SELECT("l.asset_group", "l.asset_id", "l.part_id", "COALESCE(r.exam_num,0)");
+      FROM("(".concat(new SQL() {{
+        SELECT(" ai.id as asset_id", "pm.asset_group", "pm.part_id");
+        FROM("asset_info ai");
+        JOIN("(select distinct asset_group, part_id from proc_map) as pm on ai.asset_group = pm.asset_group");
+        WHERE("ai.site_id = :site");
+        WHERE("ai.hospital_id = :hospital");
+        if (Option.of(type).filter(i -> i > 0).isDefined()) {
+          WHERE("ai.asset_group = :type");
+        }
+        if (Option.of(dept).filter(i -> i > 0).isDefined()) {
+          WHERE("ai.clinical_dept_id = :dept");
+        }
+        if (Option.of(asset).filter(i -> i > 0).isDefined()) {
+          WHERE("ai.id = :asset");
+        }
+        if (Option.of(part).filter(i -> i > 0).isDefined()) {
+          WHERE("pm.part_id = :part");
+        }
+      }}.toString()).concat(") as l"));
       LEFT_OUTER_JOIN("(".concat(new SQL() {{
         SELECT("asset_group", "asset_id", "part_id", "sum(exam_count) as exam_num");
         FROM("exam_summit");
@@ -93,7 +117,7 @@ public class ScanService {
           WHERE("part_id = :part");
         }
         GROUP_BY("asset_group", "asset_id", "part_id");
-      }}.toString()).concat(") as r on l.asset_group = r.asset_group and l.part_id = r.part_id"));
+      }}.toString()).concat(") as r on l.asset_group = r.asset_group and l.asset_id = r.asset_id and l.part_id = r.part_id"));
       ORDER_BY("l.asset_group", "l.part_id");
     }}.toString()).parameter("site", site).parameter("hospital", hospital).parameter("from", from).parameter("to", to);
     return Option.of(builder).filter(s -> Option.of(type).isDefined()).map(o -> o.parameter("type", type))
@@ -110,7 +134,19 @@ public class ScanService {
   public Observable<Tuple4<Integer, Integer, Integer, Integer>> stepDetail(int site, int hospital, Date from, Date to, Integer type, Integer dept, Integer asset, Integer part, Integer step) {
     QuerySelect.Builder builder = db.select(new SQL() {{
       SELECT("l.asset_group", "l.part_id", "l.step_id", "COALESCE(r.exam_num,0)");
-      FROM("(SELECT DISTINCT asset_group, part_id, step_id from proc_map) as l");
+      FROM("(".concat(new SQL() {{
+        SELECT_DISTINCT("asset_group", "part_id", "step_id");
+        FROM("proc_map");
+        if (Option.of(type).filter(i -> i > 0).isDefined()) {
+          WHERE("asset_group = :type");
+        }
+        if (Option.of(part).filter(i -> i > 0).isDefined()) {
+          WHERE("part_id = :part");
+        }
+        if (Option.of(step).filter(i -> i > 0).isDefined()) {
+          WHERE("step_id = :step");
+        }
+      }}.toString()).concat(") as l"));
       LEFT_OUTER_JOIN("(".concat(new SQL() {{
         SELECT("asset_group", "part_id", "step_id", "sum(exam_count) as exam_num");
         FROM("exam_summit");
