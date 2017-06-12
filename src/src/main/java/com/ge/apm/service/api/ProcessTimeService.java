9db31f3cecf5848ce7efbe70d23b17cf21c878ca
212579464 <qingtao.shen@ge.com>
2017-06-12 13:17:05 +0800

    update work_order table to v2

--- src/src/main/java/com/ge/apm/service/api/ProcessTimeService.java
+++ src/src/main/java/com/ge/apm/service/api/ProcessTimeService.java
@@ -10,7 +10,6 @@ import com.github.davidmoten.rx.jdbc.Database;
 import com.google.common.collect.ImmutableMap;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javaslang.Tuple1;
 import javaslang.Tuple4;
 import javaslang.Tuple5;
 import javaslang.Tuple7;
@@ -32,48 +31,48 @@ public class ProcessTimeService {
     private Database db;
 
     private static final String SQL_PROCESS_TIME_BY_GROUP = "select :groupby as id,cast(avg(wst.responseTime)as integer) as avg_respond,cast(avg(wst.arrivedTime)as integer) as avg_arrived, cast(avg(wst.ETTRTime)as integer) as avg_ETTR "
-            + "from asset_info ai, work_order wo, (select work_order_id,count(*) as count,sum(case  when step_id <=2 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as responseTime,"
+            + "from asset_info ai, v2_work_order wo, (select wo_id,count(*) as count,sum(case  when step_id <=2 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as responseTime,"
             + "sum(case  when step_id <=3 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as arrivedTime,"
-            + "sum(case  when step_id <=5 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
-            + "from work_order_step ws where start_time > :start_time and end_time < :end_time   group by work_order_id ) wst "
-            + "where ai.id=wo.asset_id and wo.id=wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier group by :groupby order by :orderBy :limit :offset";
+            + "sum(case  when step_id <=7 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
+            + "from v2_work_order_step ws where start_time > :start_time and end_time < :end_time   group by wo_id ) wst "
+            + "where ai.id=wo.asset_id and wo.id=wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier group by :groupby order by :orderBy :limit :offset";
 
     private static final String SQL_PROCESS_TIME_BY_ASSET = "select ai.id,ai.name ,cast(avg(wst.responseTime)as integer) as avg_respond,cast(avg(wst.arrivedTime)as integer) as avg_arrived, cast(avg(wst.ETTRTime)as integer) as avg_ETTR, "
             + "cast(avg(wst.dispatchTime)as integer) as dispatchTime, cast(avg(wst.workingTime)as integer) as workingTime "
-            + "from asset_info ai, work_order wo, (select work_order_id,count(*) as count,sum(case  when step_id <=1 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as dispatchTime,"
+            + "from asset_info ai, v2_work_order wo, (select wo_id,count(*) as count,sum(case  when step_id <=1 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as dispatchTime,"
             + "sum(case  when step_id <=2 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as responseTime,"
             + "sum(case  when step_id <=3 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as arrivedTime,"
             + "sum(case  when step_id <=4 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as workingTime,"
-            + "sum(case  when step_id <=5 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
-            + "from work_order_step ws where start_time> :start_time and end_time< :end_time   group by work_order_id ) wst "
-            + "where ai.id=wo.asset_id and wo.id=wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier group by ai.id order by :orderBy :limit :offset ";
+            + "sum(case  when step_id <=7 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
+            + "from v2_work_order_step ws where start_time> :start_time and end_time< :end_time   group by wo_id ) wst "
+            + "where ai.id=wo.asset_id and wo.id=wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier group by ai.id order by :orderBy :limit :offset ";
 
     private static final String SQL_PROCESS_TIME_GROSS = "select cast(avg(wst.responseTime)as integer) as avg_respond,cast(avg(wst.arrivedTime)as integer) as avg_arrived, cast(avg(wst.ETTRTime)as integer) as avg_ETTR ,"
             + "cast(avg(wst.dispatchTime)as integer) as dispatchTime, cast(avg(wst.workingTime)as integer) as workingTime "
-            + "from asset_info ai, work_order wo, (select work_order_id,count(*) as count,sum(case  when step_id <=1 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as dispatchTime,"
+            + "from asset_info ai, v2_work_order wo, (select wo_id,count(*) as count,sum(case  when step_id <=1 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as dispatchTime,"
             + "sum(case  when step_id <=2 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as responseTime,"
             + "sum(case  when step_id <=3 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as arrivedTime,"
             + "sum(case  when step_id <=4 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as workingTime,"
-            + "sum(case  when step_id <=5 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
-            + "from work_order_step ws where start_time> :start_time and end_time< :end_time   group by work_order_id ) wst "
-            + "where ai.id=wo.asset_id and wo.id=wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier ";
+            + "sum(case  when step_id <=7 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
+            + "from v2_work_order_step ws where start_time> :start_time and end_time< :end_time   group by wo_id ) wst "
+            + "where ai.id=wo.asset_id and wo.id=wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier ";
 
-    private static final String SQL_PROCESS_TIME_COUNT = "select count(*) from (select :groupby  from asset_info ai, work_order wo, (select work_order_id"
-            + "  from work_order_step ws where start_time> :start_time and end_time< :end_time  group by work_order_id ) wst "
-            + "  where ai.id=wo.asset_id and wo.id=wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditions group by :groupby) t1";
+    private static final String SQL_PROCESS_TIME_COUNT = "select count(*) from (select :groupby  from asset_info ai, v2_work_order wo, (select wo_id"
+            + "  from v2_work_order_step ws where start_time> :start_time and end_time< :end_time  group by wo_id ) wst "
+            + "  where ai.id=wo.asset_id and wo.id=wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditions group by :groupby) t1";
 
-    private static final String SQL_PROCESS_TIME_DISTRIBUTION ="select ETTRTime from asset_info ai, work_order wo, (select work_order_id, sum(case when step_id <= 5 then extract (EPOCH from ws.end_time - ws.start_time) end) as ETTRTime "
-                + " from work_order_step ws where start_time>:start_time and end_time<:end_time  "
-                + " group by work_order_id )wst where ai.id = wo.asset_id and wo.id = wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id"
+    private static final String SQL_PROCESS_TIME_DISTRIBUTION ="select ETTRTime from asset_info ai, v2_work_order wo, (select wo_id, sum(case when step_id <= 7 then extract (EPOCH from ws.end_time - ws.start_time) end) as ETTRTime "
+                + " from v2_work_order_step ws where start_time>:start_time and end_time<:end_time  "
+                + " group by wo_id )wst where ai.id = wo.asset_id and wo.id = wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id"
                 + " :conditionType :conditionDept :conditionSupplier order by ETTRTime limit 1 offset :offset";
     
     private static final String SQL_PROCESS_TIME_PHASE_COUNTS="select sum(case when :timePhase<:time1 then 1 end) as countT1,sum(case when :timePhase<:time2 then 1 end) as countT2,"
-                + "sum(case when :timePhase<:timeMax then 1 end) as countTMax,count(*) as countAll from asset_info ai, work_order wo, (select work_order_id,"
+                + "sum(case when :timePhase<:timeMax then 1 end) as countTMax,count(*) as countAll from asset_info ai, v2_work_order wo, (select wo_id,"
                 + "sum(case  when step_id <=2 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as responseTime,"
                 + "sum(case  when step_id <=3 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as arrivedTime,"
-                + "sum(case  when step_id <=5 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
-                + "from work_order_step ws where start_time> :start_time and end_time< :end_time   group by work_order_id ) wst "
-                + "where ai.id=wo.asset_id and wo.id=wst.work_order_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier :conditionAsset";
+                + "sum(case  when step_id <=7 then extract(epoch FROM(ws.end_time-ws.start_time))  end) as ETTRTime "
+                + "from v2_work_order_step ws where start_time> :start_time and end_time< :end_time   group by wo_id ) wst "
+                + "where ai.id=wo.asset_id and wo.id=wst.wo_id and ai.site_id=:site_id and ai.hospital_id=:hospital_id :conditionType :conditionDept :conditionSupplier :conditionAsset";
     
     
     @Resource(name = "connectionProvider")
@@ -86,7 +85,7 @@ public class ProcessTimeService {
 
     @Cacheable(cacheNames = "springCache", key = "'processTimeService.queryCount.'+#siteId+'.'+#hospitalId+'.'+#typeId+'.'+#deptId+'.'+#supplier+'.'+#fromTime+'.'+#toTime+'.groupby'+#groupby")
     public Observable<Integer> queryCount(Integer siteId, Integer hospitalId, DateTime fromTime, DateTime toTime, String groupby, Integer typeId, Integer deptId, Integer supplier) {
-        return db.select(SQL_PROCESS_TIME_COUNT.replace(":groupby", ImmutableMap.of("type", "ai.asset_group", "dept", "ai.clinical_dept_name,ai.clinical_dept_id", "supplier", "ai.supplier_id", "assetId", "ai.id","WorkOrder","work_order_id").get(groupby))
+        return db.select(SQL_PROCESS_TIME_COUNT.replace(":groupby", ImmutableMap.of("type", "ai.asset_group", "dept", "ai.clinical_dept_name,ai.clinical_dept_id", "supplier", "ai.supplier_id", "assetId", "ai.id","WorkOrder","wo_id").get(groupby))
                 .replace(":conditions", "".concat(typeId > 0 ? " and ai.asset_group=" + typeId : "").concat(deptId > 0 ? "and ai.clinical_dept_id=" + deptId : "").concat(supplier > 0 ? "and ai.supplier_id=" + supplier : "")))
                 .parameter("site_id", siteId).parameter("hospital_id", hospitalId)
                 .parameter("start_time", fromTime.toDate()).parameter("end_time", toTime.toDate())
