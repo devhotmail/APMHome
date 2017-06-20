ff20e0b1fce100d014177b8cf4f7cc611a4ea512
Xiu, Zhu <zhu.xiu@ge.com>
2017-06-20 21:34:14 +0800

    Fix: API_HOST for local build

--- src/react/MaintenanceCost/tools/webpack.prod.conf.js
+++ src/react/MaintenanceCost/tools/webpack.prod.conf.js
@@ -132,7 +132,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
-      'process.env': config.production.env
+      'process.env': process.env.NODE_ENV === 'local' ? config.watch.env : config.production.env
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
