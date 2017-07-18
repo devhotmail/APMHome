31e007154cf01c9706653b97ff07833eefdbbfc3
Ivar <ivar.chen@ge.com>
2017-07-18 14:08:53 +0800

    fixed nullpointer exception when using autoMap(Asset.class)

--- src/src/main/java/com/ge/apm/service/api/AssetsService.java
+++ src/src/main/java/com/ge/apm/service/api/AssetsService.java
@@ -4,11 +4,9 @@ import com.ge.apm.service.utils.CNY;
 import com.github.davidmoten.rx.jdbc.ConnectionProvider;
 import com.github.davidmoten.rx.jdbc.Database;
 import com.github.davidmoten.rx.jdbc.QuerySelect;
-import com.github.davidmoten.rx.jdbc.annotations.Column;
 import javaslang.Tuple;
 import javaslang.Tuple8;
 import javaslang.control.Option;
-import javaslang.control.Try;
 import org.apache.ibatis.jdbc.SQL;
 import org.javamoney.moneta.Money;
 import org.slf4j.Logger;
@@ -19,7 +17,6 @@ import rx.Observable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import java.sql.Date;
 
 @Service
 public class AssetsService {
@@ -34,33 +31,6 @@ public class AssetsService {
     db = Database.from(connectionProvider);
   }
 
-  interface Asset {
-    @Column
-    int id();
-
-    @Column
-    String name();
-
-    @Column
-    int func();
-
-    @Column
-    int type();
-
-    @Column
-    int dept();
-
-    @Column
-    int supplier();
-
-    @Column
-    double price();
-
-    @Column
-    Date yoi();
-  }
-
-
   @Cacheable(cacheNames = "springCache", key = "'assetsService.findAssets.'+#siteId+'.'+#hospitalId+'.'+#dept+'.orderBy'+#orderBy")
   public Observable<Tuple8<Integer, String, Integer, Integer, Integer, Integer, Money, Integer>> findAssets(Integer siteId, Integer hospitalId, Integer dept, String orderBy) {
     final String sql = new SQL() {{
@@ -74,8 +44,8 @@ public class AssetsService {
       ORDER_BY(Option.of(orderBy).getOrElse("id"));
     }}.toString();
     QuerySelect.Builder builder = db.select(sql).parameter("site_id", siteId).parameter("hospital_id", hospitalId);
-    return Option.of(builder).filter(s -> Option.of(dept).filter(d -> d > 0).isDefined()).map(b -> b.parameter("dept", dept)).orElse(Option.of(builder)).get().autoMap(Asset.class)
-      .map(a -> Tuple.of(a.id(), a.name(), Try.of(a::func).getOrElse(0), Try.of(a::type).getOrElse(0), Try.of(a::dept).getOrElse(0), Try.of(a::supplier).getOrElse(0), CNY.money(Option.of(a.price()).getOrElse(-1D)), Option.of(a.yoi()).map(d -> d.toLocalDate().getYear()).getOrElse(-1)))
+    return Option.of(builder).filter(s -> Option.of(dept).filter(d -> d > 0).isDefined()).map(b -> b.parameter("dept", dept)).orElse(Option.of(builder)).get()
+      .get(rs -> Tuple.of(rs.getInt("id"), rs.getString("name"), Option.of(rs.getInt("func")).getOrElse(0), Option.of(rs.getInt("type")).getOrElse(0), Option.of(rs.getInt("dept")).getOrElse(0), Option.of(rs.getInt("supplier")).getOrElse(0), CNY.money(Option.of(rs.getDouble("price")).getOrElse(-1D)), Option.of(rs.getDate("yoi")).map(d -> d.toLocalDate().getYear()).getOrElse(-1)))
       .cache();
   }
 }
