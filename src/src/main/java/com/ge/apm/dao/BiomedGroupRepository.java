f0d2505788d51b6df2d484816fc8164d2fb3a08d
gaolei <lsg@gaoleis-MacBook-Pro.local>
2017-07-26 16:48:17 -0700

    work flow web v1

--- src/src/main/java/com/ge/apm/dao/BiomedGroupRepository.java
+++ src/src/main/java/com/ge/apm/dao/BiomedGroupRepository.java
@@ -1,10 +1,13 @@
 package com.ge.apm.dao;
 
 import com.ge.apm.domain.BiomedGroup;
+import org.springframework.data.jpa.repository.Query;
 import webapp.framework.dao.GenericRepository;
 
 /**
  * Created by 212605082 on 2017/5/12.
  */
 public interface BiomedGroupRepository extends GenericRepository<BiomedGroup> {
+    @Query("select count(*) from BiomedGroup bg where bg.siteId=?1 and bg.hospitalId=?2")
+    public Integer getCountByHospital(Integer siteId,Integer hospitalId);
 }
--- src/src/main/java/com/ge/apm/view/wo/WorkOrderDispatchController.java
+++ src/src/main/java/com/ge/apm/view/wo/WorkOrderDispatchController.java
@@ -5,6 +5,7 @@
  */
 package com.ge.apm.view.wo;
 
+import com.ge.apm.dao.BiomedGroupRepository;
 import com.ge.apm.dao.ServiceRequestRepository;
 import com.ge.apm.dao.V2_WorkOrderRepository;
 import com.ge.apm.domain.UserAccount;
@@ -14,7 +15,12 @@ import com.ge.apm.domain.V2_WorkOrder_Detail;
 import com.ge.apm.domain.V2_WorkOrder_Step;
 import com.ge.apm.service.wo.V2_WorkOrderService;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
+
+import com.ge.apm.view.sysutil.UserContextService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import webapp.framework.dao.GenericRepositoryUUID;
 import webapp.framework.web.mvc.GenericCRUDUUIDController;
 import javax.faces.bean.ManagedBean;
@@ -37,19 +43,23 @@ public class WorkOrderDispatchController extends GenericCRUDUUIDController<V2_Wo
     private ServiceRequestRepository srDao;
     private V2_WorkOrderService woService;
 
-    //status：0-待派工/1-待接单/2-维修中/3-已完成/4-已派工/5-已取消
+    //status：0-待派工/1-待接单/2-维修中/3-已完成/4-已派工/10-已取消  待验收-5, 待关单-6, 已关单-7
     private Integer queryIndex;
+
+    private Integer category;
     
     private V2_ServiceRequest selectedServiceRequest;
-    
+    private BiomedGroupRepository groupDao;
     private V2_WorkOrder selectedWorkOrder;
-
+    UserAccount ua;
     protected void init() {
         dao = WebUtil.getBean(V2_WorkOrderRepository.class);
         srDao = WebUtil.getBean(ServiceRequestRepository.class);
         woService = WebUtil.getBean(V2_WorkOrderService.class);
+        groupDao = WebUtil.getBean(BiomedGroupRepository.class);
         this.filterBySite = true;
         queryIndex = 2;
+        ua = UserContextService.getCurrentUserAccount();
 
     }
 
@@ -58,13 +68,36 @@ public class WorkOrderDispatchController extends GenericCRUDUUIDController<V2_Wo
         return dao;
     }
 
+
     @Override
     protected Page<V2_WorkOrder> loadData(PageRequest pageRequest) {
+        Page<V2_WorkOrder> pages = null;
         this.setSiteFilter();
         removeFilterOnField("status");
-        if (queryIndex == 5) {
+
+        if (queryIndex == 10) {
             searchFilters.add(new SearchFilter("status", SearchFilter.Operator.EQ, 3));
+        }
+        if (queryIndex == 7) {
+            searchFilters.add(new SearchFilter("status", SearchFilter.Operator.EQ, 2));
+            removeFilterOnField("currentStepId");
+            searchFilters.add(new SearchFilter("currentStepId", SearchFilter.Operator.EQ, queryIndex));
         } else {
+           /* if(queryIndex==3){
+                Integer groupCount = groupDao.getCountByHospital(ua.getSiteId(),ua.getHospitalId());
+                Sort sort = new Sort(Sort.Direction.DESC, "current_step_id");
+                PageRequest pr = new PageRequest(pageRequest.getPageNumber(), pageRequest.getPageSize(), sort);
+                pages= dao.fetchAvailableWorkOrderByUser(ua.getId(),pr);
+                pages= dao.findByHospitalIdAndStatusAndCurrentStepIdAndCurrentPersonIdIn(ua.getHospitalId(),1,3, Arrays.asList(-1,ua.getId()),pageRequest);
+                switch(category){
+                    case 11: pages = groupCount>0? dao.fetchAvailableWorkOrderByUser(ua.getId(),pageRequest):dao.findByHospitalIdAndStatusAndCurrentStepIdAndCurrentPersonIdIn(ua.getHospitalId(),1,3, Arrays.asList(-1,ua.getId()),pageRequest); break;
+                    case 12: pages =
+
+                }
+                return pages;
+            }*/
+
+
             searchFilters.add(new SearchFilter("status", SearchFilter.Operator.EQ, 1));
             removeFilterOnField("currentStepId");
             searchFilters.add(new SearchFilter("currentStepId", SearchFilter.Operator.EQ, queryIndex));
@@ -75,8 +108,14 @@ public class WorkOrderDispatchController extends GenericCRUDUUIDController<V2_Wo
         } else {
             return dao.findBySearchFilter(this.searchFilters, pageRequest);
         }
+
+
     }
 
+    public void defineCategory(Integer cate){
+        this.category = cate;
+        cancel();
+    }
     public void searchList(Integer buttonIndex) {
         queryIndex = buttonIndex;
         cancel();
@@ -85,6 +124,14 @@ public class WorkOrderDispatchController extends GenericCRUDUUIDController<V2_Wo
     public void cancel() {
         selected=null;
         selectedServiceRequest = null;
+    }
+
+    public Integer getCategory() {
+        return category;
+    }
+
+    public void setCategory(Integer category) {
+        this.category = category;
     }
 
     public V2_ServiceRequest getServiceRequest(String srId){
